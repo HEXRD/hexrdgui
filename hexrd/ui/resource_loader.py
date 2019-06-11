a62311d6dd22949c83aa338803621d3e0e951b97
Patrick Avery <patrick.avery@kitware.com>
2019-06-11 12:09:21 -0400

    Add option for resource loader to read in bytes
    This allows users to read in bytes from the resource loader.
    This is useful if the resource needs to be in a bytes format.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/resource_loader.py
+++ hexrd/ui/resource_loader.py
@@ -4,6 +4,9 @@ except ImportError:
     # Try backported to PY<37 `importlib_resources`.
     import importlib_resources
 
-def load_resource(module, name):
+def load_resource(module, name, binary=False):
     """This will return a string of a resource's contents"""
+    if binary:
+        return importlib_resources.read_binary(module, name)
+
     return importlib_resources.read_text(module, name)
