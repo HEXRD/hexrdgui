47ff294307edadb034a317dd03e06f111096c678
Patrick Avery <patrick.avery@kitware.com>
2019-06-24 12:05:43 -0400

    Add live updates for materials panel changes
    Now, when the user modifies the materials panel or the beam
    energy, the rings in the canvas are automatically updated.
    This is done by the configuration emitting a signal, the canvases
    checking to see if they are currently displaying a calibration,
    and then updating the rings if they are.
    
    This also restructures the canvases slightly so that, when they
    are displaying a calibration, they store in memory an
    InstrumentViewer object, so they do not need to re-create a lot
    of objects (such as the hexrd instrument object) when updating
    the rings. It re-uses the InstrumentViewer to update the rings.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/calibration/cartesian_plot.py
+++ hexrd/ui/calibration/cartesian_plot.py
@@ -14,8 +14,7 @@ from skimage.exposure import equalize_adapthist
 
 from .display_plane import DisplayPlane
 
-
-def cartesian_image():
+def cartesian_viewer():
     iconfig = HexrdConfig().instrument_config
     images_dict = HexrdConfig().images()
     plane_data = HexrdConfig().active_material.planeData
@@ -30,120 +29,105 @@ def cartesian_image():
                'PanelIds: ' + str(list(instr._detectors.keys())))
         raise Exception(msg)
 
-    dplane = DisplayPlane()
-    dpanel = make_dpanel(dplane, instr, pixel_size)
-
-    ring_data = add_rings(dpanel, plane_data)
-
-    img = plot_dplane(dpanel, images_dict, instr, dplane)
-
-    # Rescale the data to match the scale of the original dataset
-    # TODO: try to get create_calibration_image to not rescale the
-    # result to be between 0 and 1 in the first place so this will
-    # not be necessary.
-    images = images_dict.values()
-    minimum = min([x.min() for x in images])
-    maximum = max([x.max() for x in images])
-    img = np.interp(img, (img.min(), img.max()), (minimum, maximum))
-
-    return img, ring_data
+    return InstrumentViewer(instr, images_dict, plane_data, pixel_size)
 
 
-def load_pdata(data, key, tth_max=None):
-    """
-    tth_max is in DEGREES
-    """
-    # If the pickle file was created with python2, we must use
-    # latin1 encoding to read it for some reason.
-    matlist = pickle.loads(data, encoding='latin1')
+class InstrumentViewer:
 
-    pd = dict(zip([i.name for i in matlist], matlist))[key].planeData
-    if tth_max is not None:
-        pd.exclusions = np.zeros_like(pd.exclusions, dtype=bool)
-        pd.tThMax = np.radians(tth_max)
-    return pd
+    def __init__(self, instr, images_dict, plane_data, pixel_size):
+        self.type = 'cartesian'
+        self.instr = instr
+        self.images_dict = images_dict
+        self.plane_data = plane_data
+        self.pixel_size = pixel_size
 
+        self.dplane = DisplayPlane()
+        self.make_dpanel()
+        self.plot_dplane()
 
-def load_ceo2():
-    materials = resource_loader.load_resource(hexrd.ui.resources.materials,
-                                              'materials.hexrd', binary=True)
-    return load_pdata(materials, 'ceo2')
+    def make_dpanel(self):
+        dpanel_sizes = self.dplane.panel_size(self.instr)
+        self.dpanel = self.dplane.display_panel(dpanel_sizes, self.pixel_size)
 
+    def clear_rings(self):
+        self.ring_data = []
 
-def make_dpanel(dplane, instr, pixel_size):
-    dpanel_sizes = dplane.panel_size(instr)
-    dpanel = dplane.display_panel(dpanel_sizes, pixel_size)
-    return dpanel
+    def add_rings(self):
+        self.clear_rings()
+        if not HexrdConfig().show_rings:
+            # We are not supposed to add rings
+            return self.ring_data
 
+        selected_rings = HexrdConfig().selected_rings
+        if selected_rings:
+            # We should only get specific values
+            tth = self.plane_data.getTTh()
+            tth = [tth[i] for i in selected_rings]
+            delta_tth = np.degrees(self.plane_data.tThWidth)
 
-def add_rings(dpanel, plane_data):
-    ring_data = []
-    if not HexrdConfig().show_rings:
-        # We are not supposed to add rings
-        return ring_data
+            ring_angs, ring_xys = self.dpanel.make_powder_rings(
+                tth, delta_tth=delta_tth, delta_eta=1)
+        else:
+            ring_angs, ring_xys = self.dpanel.make_powder_rings(
+                self.plane_data, delta_eta=1)
 
-    selected_rings = HexrdConfig().selected_rings
-    if selected_rings:
-        # We should only get specific values
-        tth = plane_data.getTTh()
-        tth = [tth[i] for i in selected_rings]
-        delta_tth = np.degrees(plane_data.tThWidth)
+        for ring in ring_xys:
+            self.ring_data.append(self.dpanel.cartToPixel(ring))
 
-        ring_angs, ring_xys = dpanel.make_powder_rings(
-            tth, delta_tth=delta_tth, delta_eta=1)
-    else:
-        ring_angs, ring_xys = dpanel.make_powder_rings(
-            plane_data, delta_eta=1)
+        return self.ring_data
 
-    for ring in ring_xys:
-        ring_data.append(dpanel.cartToPixel(ring))
+    def plot_dplane(self):
+        nrows_map = self.dpanel.rows
+        ncols_map = self.dpanel.cols
+        warped = np.zeros((nrows_map, ncols_map))
+        for detector_id in self.images_dict.keys():
+            img = self.images_dict[detector_id]
 
-    return ring_data
+            max_int = np.percentile(img, 99.95)
+            pbuf = 10
+            img[:, :pbuf] = max_int
+            img[:, -pbuf:] = max_int
+            img[:pbuf, :] = max_int
+            img[-pbuf:, :] = max_int
+            panel = self.instr._detectors[detector_id]
 
+            # map corners
+            corners = np.vstack(
+                [panel.corner_ll,
+                 panel.corner_lr,
+                 panel.corner_ur,
+                 panel.corner_ul,
+                 ]
+            )
+            mp = panel.map_to_plane(corners, self.dplane.rmat,
+                                    self.dplane.tvec)
 
-def plot_dplane(dpanel, images_dict, instr, dplane):
-    nrows_map = dpanel.rows
-    ncols_map = dpanel.cols
-    warped = np.zeros((nrows_map, ncols_map))
-    for detector_id in images_dict.keys():
-        img = images_dict[detector_id]
+            col_edges = self.dpanel.col_edge_vec
+            row_edges = self.dpanel.row_edge_vec
+            j_col = cellIndices(col_edges, mp[:, 0])
+            i_row = cellIndices(row_edges, mp[:, 1])
 
-        max_int = np.percentile(img, 99.95)
-        pbuf = 10
-        img[:, :pbuf] = max_int
-        img[:, -pbuf:] = max_int
-        img[:pbuf, :] = max_int
-        img[-pbuf:, :] = max_int
-        panel = instr._detectors[detector_id]
+            src = np.vstack([j_col, i_row]).T
+            dst = panel.cartToPixel(corners, pixels=True)
+            dst = dst[:, ::-1]
 
-        # map corners
-        corners = np.vstack(
-            [panel.corner_ll,
-             panel.corner_lr,
-             panel.corner_ur,
-             panel.corner_ul,
-             ]
-        )
-        mp = panel.map_to_plane(corners, dplane.rmat, dplane.tvec)
+            tform3 = tf.ProjectiveTransform()
+            tform3.estimate(src, dst)
 
-        col_edges = dpanel.col_edge_vec
-        row_edges = dpanel.row_edge_vec
-        j_col = cellIndices(col_edges, mp[:, 0])
-        i_row = cellIndices(row_edges, mp[:, 1])
+            warped += tf.warp(img, tform3,
+                              output_shape=(self.dpanel.rows,
+                                            self.dpanel.cols))
 
-        src = np.vstack([j_col, i_row]).T
-        dst = panel.cartToPixel(corners, pixels=True)
-        dst = dst[:, ::-1]
+        """
+        IMAGE PLOTTING AND LIMIT CALCULATION
+        """
+        img = equalize_adapthist(warped, clip_limit=0.1, nbins=2**16)
 
-        tform3 = tf.ProjectiveTransform()
-        tform3.estimate(src, dst)
-
-        warped += tf.warp(img, tform3,
-                          output_shape=(dpanel.rows,
-                                        dpanel.cols))
-
-    """
-    IMAGE PLOTTING AND LIMIT CALCULATION
-    """
-    img = equalize_adapthist(warped, clip_limit=0.1, nbins=2**16)
-    return img
+        # Rescale the data to match the scale of the original dataset
+        # TODO: try to get create_calibration_image to not rescale the
+        # result to be between 0 and 1 in the first place so this will
+        # not be necessary.
+        images = self.images_dict.values()
+        minimum = min([x.min() for x in images])
+        maximum = max([x.max() for x in images])
+        self.img = np.interp(img, (img.min(), img.max()), (minimum, maximum))
