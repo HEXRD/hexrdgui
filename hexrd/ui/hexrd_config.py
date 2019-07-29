e9bd23d6add826e9538adaffbdc88fa5ff56c64b
Patrick Avery <patrick.avery@kitware.com>
2019-07-29 18:53:56 -0400

    Added ability to display saturation percentages
    When in image viewer mode, a user can now choose to display
    saturation percentages in the bottom left corners of the images.
    
    If a user checks the box, "View"->"Show Saturation Percentages",
    saturation percentages will be displayed in white text in the
    bottom left corner of every image. The saturation percentages are
    calculated using the detector's "saturation_level" setting.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/hexrd_config.py
+++ hexrd/ui/hexrd_config.py
@@ -45,6 +45,9 @@ class HexrdConfig(QObject, metaclass=Singleton):
     """Emitted when ring configuration has changed"""
     ring_config_changed = Signal()
 
+    """Emitted when the option to show the saturation level is changed"""
+    show_saturation_level_changed = Signal()
+
     def __init__(self):
         # Should this have a parent?
         super(HexrdConfig, self).__init__(None)
@@ -57,6 +60,7 @@ class HexrdConfig(QObject, metaclass=Singleton):
         self.images_dict = {}
         self.hdf5_path = []
         self.live_update = False
+        self._show_saturation_level = False
 
         self.load_settings()
 
@@ -518,3 +522,14 @@ class HexrdConfig(QObject, metaclass=Singleton):
 
     cartesian_pixel_size = property(_cartesian_pixel_size,
                                     _set_cartesian_pixel_size)
+
+    def get_show_saturation_level(self):
+        return self._show_saturation_level
+
+    def set_show_saturation_level(self, v):
+        if self._show_saturation_level != v:
+            self._show_saturation_level = v
+            self.show_saturation_level_changed.emit()
+
+    show_saturation_level = property(get_show_saturation_level,
+                                     set_show_saturation_level)
