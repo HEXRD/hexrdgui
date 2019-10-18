d8b0c9e86f8363b8a7323c6902b86761587bc047
Patrick Avery <patrick.avery@kitware.com>
2019-10-18 15:16:30 -0400

    Add sliders for beam config
    This adds sliders for the beam energy and its azimuth and
    polar angles. Modifying the beam energy results in a live
    update of the rings.
    
    Currently, modifying the azimuth and polar angles do not
    perform any updates, and it is something I will need to add
    soon.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/calibration_slider_widget.py
+++ hexrd/ui/calibration_slider_widget.py
@@ -27,12 +27,13 @@ class CalibrationSliderWidget(QObject):
     def setup_connections(self):
         self.ui.detector.currentIndexChanged.connect(
             self.update_gui_from_config)
-        for widget in self.transform_widgets():
+        for widget in self.config_widgets():
             widget.valueChanged.connect(self.update_widget_counterpart)
             widget.valueChanged.connect(self.update_config_from_gui)
 
         self.ui.sb_translation_range.valueChanged.connect(self.update_ranges)
         self.ui.sb_tilt_range.valueChanged.connect(self.update_ranges)
+        self.ui.sb_beam_range.valueChanged.connect(self.update_ranges)
 
     def update_ranges(self):
         r = self.ui.sb_translation_range.value()
@@ -45,6 +46,13 @@ class CalibrationSliderWidget(QObject):
         r = self.ui.sb_tilt_range.value()
         slider_r = r * self.CONF_VAL_TO_SLIDER_VAL
         for w in self.tilt_widgets():
+            v = w.value()
+            r_val = slider_r if w.objectName().startswith('slider') else r
+            w.setRange(v - r_val / 2.0, v + r_val / 2.0)
+
+        r = self.ui.sb_beam_range.value()
+        slider_r = r * self.CONF_VAL_TO_SLIDER_VAL
+        for w in self.beam_widgets():
             v = w.value()
             r_val = slider_r if w.objectName().startswith('slider') else r
             w.setRange(v - r_val / 2.0, v + r_val / 2.0)
@@ -84,8 +92,25 @@ class CalibrationSliderWidget(QObject):
     def transform_widgets(self):
         return self.translation_widgets() + self.tilt_widgets()
 
+    def beam_widgets(self):
+        # Let's take advantage of the naming scheme
+        prefixes = ['sb', 'slider']
+        roots = ['energy', 'azimuth', 'polar']
+        suffixes = ['0']
+        widget_names = [
+            '_'.join([p, r, s])
+            for p in prefixes
+            for r in roots
+            for s in suffixes
+        ]
+
+        return [getattr(self.ui, x) for x in widget_names]
+
+    def config_widgets(self):
+        return self.transform_widgets() + self.beam_widgets()
+
     def all_widgets(self):
-        return self.transform_widgets() + [self.ui.detector]
+        return self.config_widgets() + [self.ui.detector]
 
     def block_all_signals(self):
         previously_blocked = []
@@ -133,29 +158,9 @@ class CalibrationSliderWidget(QObject):
 
         previously_blocked = self.block_all_signals()
         try:
-            det = self.current_detector_dict()
-            for widget in self.transform_widgets():
-                name = widget.objectName()
+            for widget in self.config_widgets():
+                self.update_widget_value(widget)
 
-                # Take advantage of the widget naming scheme
-                prefix, key, ind = name.split('_')
-                ind = int(ind)
-
-                val = det['transform'][key]['value'][ind]
-                if key == 'tilt':
-                    # Convert to degrees
-                    val = np.degrees(val)
-
-                if prefix == 'slider':
-                    val *= self.CONF_VAL_TO_SLIDER_VAL
-
-                # Make sure the widget's range will accept the value
-                if val < widget.minimum():
-                    widget.setMinimum(val)
-                elif val > widget.maximum():
-                    widget.setMaximum(val)
-
-                widget.setValue(val)
         finally:
             self.unblock_all_signals(previously_blocked)
 
@@ -188,8 +193,6 @@ class CalibrationSliderWidget(QObject):
         sender = self.sender()
         name = sender.objectName()
 
-        det = self.current_detector_dict()
-
         # Take advantage of the widget naming scheme
         prefix, key, ind = name.split('_')
         ind = int(ind)
@@ -197,13 +200,60 @@ class CalibrationSliderWidget(QObject):
         if prefix == 'slider':
             val *= self.SLIDER_VAL_TO_CONF_VAL
 
+        if key in ['tilt', 'translation']:
+            det = self.current_detector_dict()
+            if key == 'tilt':
+                # Convert to radians before saving
+                val = np.radians(val)
+
+            det['transform'][key]['value'][ind] = val
+
+            # Since we modify the value directly instead of letting the
+            # HexrdConfig() do it, let's also emit the signal it would
+            # have emitted.
+            HexrdConfig().detector_transform_modified.emit(
+                self.current_detector()
+            )
+        else:
+            iconfig = HexrdConfig().config['instrument']
+            if key == 'energy':
+                iconfig['beam'][key]['value'] = val
+                HexrdConfig().update_active_material_energy()
+            elif key == 'polar':
+                iconfig['beam']['vector']['polar_angle']['value'] = val
+            else:
+                iconfig['beam']['vector'][key]['value'] = val
+
+    def update_widget_value(self, widget):
+        name = widget.objectName()
+
+        # Take advantage of the widget naming scheme
+        prefix, key, ind = name.split('_')
+        ind = int(ind)
+
+        if key in ['tilt', 'translation']:
+            det = self.current_detector_dict()
+            val = det['transform'][key]['value'][ind]
+        else:
+            iconfig = HexrdConfig().config['instrument']
+            if key == 'energy':
+                val = iconfig['beam'][key]['value']
+            elif key == 'polar':
+                val = iconfig['beam']['vector']['polar_angle']['value']
+            else:
+                val = iconfig['beam']['vector'][key]['value']
+
         if key == 'tilt':
-            # Convert to radians before saving
-            val = np.radians(val)
+            # Convert to degrees
+            val = np.degrees(val)
 
-        det['transform'][key]['value'][ind] = val
+        if prefix == 'slider':
+            val *= self.CONF_VAL_TO_SLIDER_VAL
 
-        # Since we modify the value directly instead of letting the
-        # HexrdConfig() do it, let's also emit the signal it would
-        # have emitted.
-        HexrdConfig().detector_transform_modified.emit(self.current_detector())
+        # Make sure the widget's range will accept the value
+        if val < widget.minimum():
+            widget.setMinimum(val)
+        elif val > widget.maximum():
+            widget.setMaximum(val)
+
+        widget.setValue(val)
