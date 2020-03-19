4ed5e8243dbb0724387b8e7a0e91d74fef009e99
Chris Harris <chris.harris@kitware.com>
2020-03-19 11:10:26 -0400

    Also generate zip on Windows

--- packaging/CPackConfig.cmake
+++ packaging/CPackConfig.cmake
@@ -19,7 +19,7 @@ if (APPLE)
   set(CPACK_DMG_DS_STORE_SETUP_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/darwin/CMakeDMGSetup.scpt")
   set(CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_CURRENT_LIST_DIR}/darwin/CMakeDMGBackground.tif")
 elseif(WIN32)
-  set(CPACK_GENERATOR "WIX")
+  set(CPACK_GENERATOR "WIX;ZIP")
   set(CPACK_WIX_PROPERTY_ARPURLINFOABOUT "https://github.com/hexrd/hexrdgui")
 
   set(CPACK_WIX_PROPERTY_ARPCONTACT "hexrd@kitware.com")
