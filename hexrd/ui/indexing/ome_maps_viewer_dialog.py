0877b60abcb3de0161e3f3ea2a006b24c1408e31
Patrick Avery <patrick.avery@kitware.com>
2020-06-30 17:59:59 -0400

    Ome maps viewer is mostly complete...
    The options are now added, and seem to work. The indexing actually
    runs.
    
    We just need to fix some of the spot labelling.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/indexing/ome_maps_viewer_dialog.py
+++ hexrd/ui/indexing/ome_maps_viewer_dialog.py
@@ -3,19 +3,22 @@ import copy
 import matplotlib
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.figure import Figure
-
 import numpy as np
+from scipy import ndimage
+import yaml
 
 from PySide2.QtCore import Signal, QObject, QSignalBlocker, Qt
-from PySide2.QtWidgets import QFileDialog, QSizePolicy
+from PySide2.QtWidgets import QComboBox, QFileDialog, QSizePolicy
 
-from hexrd.instrument import GenerateEtaOmeMaps
+from hexrd.ui import resource_loader
 
 from hexrd.ui.color_map_editor import ColorMapEditor
 from hexrd.ui.hexrd_config import HexrdConfig
 from hexrd.ui.navigation_toolbar import NavigationToolbar
 from hexrd.ui.ui_loader import UiLoader
+
 import hexrd.ui.constants
+import hexrd.ui.resources.indexing
 
 
 class OmeMapsViewerDialog(QObject):
@@ -32,6 +35,19 @@ class OmeMapsViewerDialog(QObject):
         self.data = data
         self.cmap = hexrd.ui.constants.DEFAULT_CMAP
         self.norm = None
+        self.spots = None
+
+        self.setup_widget_paths()
+
+        # Set the item data for the combo box to be the names we want
+        item_data = [
+            'dbscan',
+            'sph-dbscan',
+            'ort-dbscan',
+            'fclusterdata'
+        ]
+        for i, data in enumerate(item_data):
+            self.ui.clustering_algorithm.setItemData(i, data)
 
         self.setup_plot()
         self.setup_color_map()
@@ -41,7 +57,9 @@ class OmeMapsViewerDialog(QObject):
         self.setup_connections()
 
     def setup_connections(self):
+        self.ui.active_hkl.currentIndexChanged.connect(self.clear_spots)
         self.ui.active_hkl.currentIndexChanged.connect(self.update_plot)
+        self.ui.label_spots_button.pressed.connect(self.label_spots)
         self.ui.export_button.pressed.connect(self.on_export_button_pressed)
         self.ui.accepted.connect(self.on_accepted)
         self.ui.rejected.connect(self.on_rejected)
@@ -50,10 +68,32 @@ class OmeMapsViewerDialog(QObject):
         self.ui.show()
 
     def on_accepted(self):
+        # Any validation may be performed here first
+        self.update_config()
         self.accepted.emit()
 
     def on_rejected(self):
         self.rejected.emit()
+
+    def setup_widget_paths(self):
+        text = resource_loader.load_resource(hexrd.ui.resources.indexing,
+                                             'gui_config_maps.yml')
+        self.gui_config_maps = yaml.load(text, Loader=yaml.FullLoader)
+
+        paths = {}
+
+        def recursive_get_paths(cur_config, cur_path):
+            for key, value in cur_config.items():
+                new_path = cur_path + [key]
+                if isinstance(value, str):
+                    paths[value] = new_path
+                    continue
+
+                recursive_get_paths(value, new_path)
+
+        initial_path = []
+        recursive_get_paths(self.gui_config_maps, initial_path)
+        self.widget_paths = paths
 
     def update_hkl_options(self):
         # This won't trigger a re-draw. Can change in the future if needed.
@@ -127,6 +167,15 @@ class OmeMapsViewerDialog(QObject):
 
         self.color_map_editor.update_bounds(self.image_data)
 
+    def clear_spots(self):
+        self.clear_spot_lines()
+        self.spots = None
+
+    def clear_spot_lines(self):
+        if hasattr(self, '_spot_lines'):
+            self._spot_lines.remove()
+            del self._spot_lines
+
     def update_plot(self):
         fig = self.fig
         ax = self.ax
@@ -143,6 +192,11 @@ class OmeMapsViewerDialog(QObject):
         else:
             im = self.im
             im.set_data(data)
+
+        self.clear_spot_lines()
+        if self.spots is not None:
+            self._spot_lines = ax.scatter(self.spots[:, 1], self.spots[:, 0],
+                                          18, 'm', '+')
 
         im.set_cmap(self.cmap)
         im.set_norm(self.norm)
@@ -166,6 +220,33 @@ class OmeMapsViewerDialog(QObject):
         self.norm = norm
         self.update_plot()
 
+    def label_spots(self):
+        # Make a deep copy to modify
+        data = copy.deepcopy(self.image_data)
+        shape = data.shape
+
+        # Get rid of nans to make our work easier
+        data[np.isnan(data)] = 0
+
+        structure = np.ones((3, 3))
+        labels, numSpots = ndimage.label(data > self.threshold, structure)
+
+        index = np.arange(np.amax(labels)) + 1
+        spots = ndimage.measurements.center_of_mass(data, labels, index)
+        spots = np.array(spots)
+
+        # Rescale the points to match the extents
+        x_length = self.extent[1] - self.extent[0]
+        y_length = self.extent[3] - self.extent[2]
+        spots[:, 1] *= x_length / shape[1]
+        spots[:, 1] += self.extent[0]
+        spots[:, 0] *= y_length / shape[0]
+        spots[:, 0] -= self.extent[2]
+
+        self.spots = spots
+
+        self.update_plot()
+
     def on_export_button_pressed(self):
         selected_file, selected_filter = QFileDialog.getSaveFileName(
             self.ui, 'Export Eta Omega Maps', HexrdConfig().working_dir,
@@ -177,10 +258,99 @@ class OmeMapsViewerDialog(QObject):
 
             self.data.save(selected_file)
 
-    def update_config(self):
-        # Update all of the config with their settings from the widgets
-        pass
+    @property
+    def threshold(self):
+        return self.color_map_editor.ui.minimum.value()
+
+    @threshold.setter
+    def threshold(self, v):
+        self.color_map_editor.ui.minimum.setValue(v)
+
+    @property
+    def yaml_widgets(self):
+        return [getattr(self.ui, x) for x in self.widget_paths.keys()]
+
+    @property
+    def all_widgets(self):
+        return self.yaml_widgets + [
+          self.ui.tab_widget,
+          self.ui.active_hkl
+        ]
 
     def update_gui(self):
         # Updates all of the widgets with their settings from the config
         self.update_hkl_options()
+        blockers = [QSignalBlocker(x) for x in self.all_widgets]  # noqa: F841
+
+        def setter(w):
+            if isinstance(w, QComboBox):
+                return lambda x: w.setCurrentIndex(w.findData(x))
+
+            # Assume it is a spin box of some kind
+            return w.setValue
+
+        config = HexrdConfig().indexing_config
+
+        def set_val(w, path):
+            cur = config
+            for x in path:
+                if x not in cur:
+                    # If it's not in the config, skip over it
+                    return
+                cur = cur[x]
+
+            setter(w)(cur)
+
+        for w, path in self.widget_paths.items():
+            w = getattr(self.ui, w)
+            set_val(w, path)
+
+        # Change the tab to the last used tab
+        # Take advantage of common naming...
+        method = config['find_orientations']['seed_search']['method']
+        method_name = next(iter(method))
+        method_widget = getattr(self.ui, method_name + '_tab')
+        self.ui.tab_widget.setCurrentWidget(method_widget)
+
+        # Also set the color map minimum to the threshold value...
+        self.threshold = config['find_orientations']['threshold']
+
+    def update_config(self):
+        # Update all of the config with their settings from the widgets
+        config = HexrdConfig().indexing_config
+
+        # Clear the method so it can be set to a different one
+        method = config['find_orientations']['seed_search']['method']
+        method.clear()
+
+        # Give it some dummy contents so the setter below will run
+        method_name = self.ui.tab_widget.currentWidget().objectName()
+        method_name = method_name.replace('_tab', '')
+        dummy_method = (
+            self.gui_config_maps['find_orientations']['seed_search']['method'])
+        method[method_name] = copy.deepcopy(dummy_method[method_name])
+
+        def getter(w):
+            if isinstance(w, QComboBox):
+                return w.currentData
+
+            # Assume it is a spin box of some kind
+            return w.value
+
+        def set_val(val, path):
+            cur = config
+            for x in path[:-1]:
+                if x not in cur:
+                    # If it's not in the config, skip over it
+                    return
+                cur = cur[x]
+
+            cur[path[-1]] = val
+
+        for w, path in self.widget_paths.items():
+            w = getattr(self.ui, w)
+            val = getter(w)()
+            set_val(val, path)
+
+        # Also set the threshold to the minimum color map value...
+        config['find_orientations']['threshold'] = self.threshold
--- hexrd/ui/indexing/run.py
+++ hexrd/ui/indexing/run.py
@@ -1,6 +1,12 @@
-from hexrd.findorientations import generate_eta_ome_maps
+import numpy as np
+
+from hexrd import indexer
+from hexrd.findorientations import (
+    generate_eta_ome_maps, generate_orientation_fibers
+)
 from hexrd.xrdutil import EtaOmeMaps
 
+from hexrd.ui.hexrd_config import HexrdConfig
 from hexrd.ui.indexing.create_config import create_indexing_config
 from hexrd.ui.indexing.ome_maps_select_dialog import OmeMapsSelectDialog
 from hexrd.ui.indexing.ome_maps_viewer_dialog import OmeMapsViewerDialog
@@ -24,7 +30,7 @@ class IndexingRunner:
         # We will go through these steps:
         # 1. Have the user select/generate eta omega maps
         # 2. Have the user view and threshold the eta omega maps
-        # 3. ...
+        # 3. Run the indexing
         self.select_ome_maps()
 
     def select_ome_maps(self):
@@ -52,14 +58,6 @@ class IndexingRunner:
     def view_ome_maps(self):
         # Now, show the Ome Map viewer
 
-        ### TEMPORARY SECTION ###
-        import hexrd.ui.indexing.ome_maps_viewer_dialog
-        import importlib
-
-        importlib.reload(hexrd.ui.indexing.ome_maps_viewer_dialog)
-        from hexrd.ui.indexing.ome_maps_viewer_dialog import OmeMapsViewerDialog
-        ### END TEMPORARY SECTION ###
-
         dialog = OmeMapsViewerDialog(self.ome_maps, self.parent)
         dialog.accepted.connect(self.ome_maps_viewed)
         dialog.rejected.connect(self.clear)
@@ -68,4 +66,32 @@ class IndexingRunner:
         self.ome_maps_viewer_dialog = dialog
 
     def ome_maps_viewed(self):
-        pass
+        # The dialog should have automatically updated our internal config
+        # Let's go ahead and run the indexing!
+
+        # For now, always use all hkls from eta omega maps
+        hkls = list(range(len(self.ome_maps.iHKLList)))
+        indexing_config = HexrdConfig().indexing_config
+        indexing_config['find_orientations']['seed_search']['hkl_seeds'] = hkls
+
+        # Create a full indexing config
+        config = create_indexing_config()
+
+        # Generate the orientation fibers
+        self.qfib = generate_orientation_fibers(config, self.ome_maps)
+
+        # Run the indexer
+        ncpus = config.multiprocessing
+        completeness = indexer.paintGrid(
+            self.qfib,
+            self.ome_maps,
+            etaRange=np.radians(config.find_orientations.eta.range),
+            omeTol=np.radians(config.find_orientations.omega.tolerance),
+            etaTol=np.radians(config.find_orientations.eta.tolerance),
+            omePeriod=np.radians(config.find_orientations.omega.period),
+            threshold=config.find_orientations.threshold,
+            doMultiProc=ncpus > 1,
+            nCPUs=ncpus
+            )
+        self.completeness = np.array(completeness)
+        print('Done!')
