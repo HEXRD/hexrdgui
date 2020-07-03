dfd82b5ef0673bd6fed8676411a71417e4eee83b
John Tourtellott <john@tortuga2.home>
2020-07-03 16:40:49 -0400

    Recompute polar masks in main window update_all()
    Also adds line data to HexrdConfig

--- hexrd/ui/create_polar_mask.py
+++ hexrd/ui/create_polar_mask.py
@@ -2,18 +2,34 @@ import numpy as np
 
 from skimage.draw import polygon
 
+from hexrd.ui import constants
 from hexrd.ui.hexrd_config import HexrdConfig
 
 
-def create_polar_mask(line_data, rsimg, pv):
+def create_polar_mask(line_data):
+    # Calculate current image dimensions
+    eta_min = constants.UI_ETA_MIN_DEGREES
+    eta_max = constants.UI_ETA_MAX_DEGREES
+    eta_range =  eta_max - eta_min
+    eta_pixel_size = HexrdConfig().polar_pixel_size_eta
+    neta = int(np.ceil(eta_range / eta_pixel_size))
+
+    tth_min = HexrdConfig().polar_res_tth_min
+    tth_max = HexrdConfig().polar_res_tth_max
+    tth_range = tth_max - tth_min
+    tth_pixel_size = HexrdConfig().polar_pixel_size_tth
+    ntth = int(np.ceil(tth_range / tth_pixel_size))
+
+    shape = (neta, ntth)
+    # Generate masks from line data
     for line in line_data:
-        x = [point[0] for point in line]
-        y = [point[1] for point in line]
+        tth = np.asarray([point[0] for point in line])
+        eta = np.asarray([point[1] for point in line])
 
-        j_col = np.floor(pv.tth_to_pixel(np.radians(x)))
-        i_row = np.floor(pv.eta_to_pixel(np.radians(y)))
+        j_col = np.floor((tth - tth_min) / tth_pixel_size)
+        i_row = np.floor((eta - eta_min) / eta_pixel_size)
 
-        rr, cc = polygon(i_row, j_col, shape=rsimg.shape)
-        mask = np.ones(rsimg.shape, dtype=bool)
+        rr, cc = polygon(i_row, j_col, shape=shape)
+        mask = np.ones(shape, dtype=bool)
         mask[rr, cc] = False
         HexrdConfig().polar_masks.append(mask)
