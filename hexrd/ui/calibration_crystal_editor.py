794792a0e7c11c5b3caf3f85e1449a521532b365
Patrick Avery <patrick.avery@kitware.com>
2020-07-15 18:15:39 -0400

    Add calibration crystal editor with stretch matrix
    This separates out the calibration crystal editor as a stand-alone
    widget, because it will be re-used for the mono rotation series.
    
    Additionally, the stretch matrix is used instead of an inverse
    stretch matrix. This is automatically converted to inverse stretch
    values behind the scenes, which then get passed to hexrd.
    
    If the user inputs values for a stretch matrix that is a noninvertible
    (singular) matrix, the matrix boxes appear as red, and the values
    are not set. But once the matrix is invertible, the values will
    appear as white again.
    
    Additionally, values across the diagonal are kept identical in the
    stretch matrix automatically.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

---
+++ hexrd/ui/calibration_crystal_editor.py
@@ -0,0 +1,212 @@
+import copy
+import numpy as np
+from numpy.linalg import LinAlgError
+
+from PySide2.QtCore import QObject, QSignalBlocker, Signal
+
+from hexrd.ui.hexrd_config import HexrdConfig
+from hexrd.ui.ui_loader import UiLoader
+from hexrd.ui.utils import convert_angle_convention
+
+
+class CalibrationCrystalEditor(QObject):
+
+    # Emitted when the params get modified
+    params_modified = Signal()
+
+    def __init__(self, params=None, parent=None):
+        super().__init__(parent)
+
+        loader = UiLoader()
+        self.ui = loader.load_file('calibration_crystal_editor.ui', parent)
+
+        self.params = params
+
+        self.update_gui()
+        self.update_orientation_suffixes()
+
+        self.setup_connections()
+
+    def setup_connections(self):
+        HexrdConfig().euler_angle_convention_changed.connect(
+            self.euler_angle_convention_changed)
+
+        for w in self.all_widgets:
+            w.valueChanged.connect(self.value_changed)
+
+    @property
+    def params(self):
+        return self._params
+
+    @params.setter
+    def params(self, v):
+        self._params = copy.deepcopy(v)
+        self.update_gui()
+
+    def value_changed(self):
+        sender = self.sender()
+
+        if sender in self.orientation_widgets:
+            self.params[:3] = self.orientation
+        elif sender in self.position_widgets:
+            self.params[3:6] = self.position
+        else:
+            # If the stretch matrix was modified, we may need to update
+            # a duplicate value in the matrix.
+            self.update_duplicate(sender)
+            try:
+                self.params[6:] = self.inverse_stretch
+            except LinAlgError:
+                self.set_matrix_invalid()
+                return
+
+            self.set_matrix_valid()
+
+        self.params_modified.emit()
+
+    def euler_angle_convention_changed(self):
+        self.update_gui()
+        self.update_orientation_suffixes()
+
+    def update_orientation_suffixes(self):
+        suffix = '' if HexrdConfig().euler_angle_convention is None else '°'
+        for w in self.orientation_widgets:
+            w.setSuffix(suffix)
+
+    def update_params(self):
+        if self.params is None:
+            return
+
+        self.params[:3] = self.orientation
+        self.params[3:6] = self.position
+        self.params[6:] = self.inverse_stretch
+        self.params_modified.emit()
+
+    def update_gui(self):
+        if self.params is None:
+            return
+
+        self.orientation = self.params[:3]
+        self.position = self.params[3:6]
+        self.inverse_stretch = self.params[6:]
+
+    @property
+    def stretch_matrix_duplicates(self):
+        return {
+            1: 3,
+            2: 6,
+            5: 7,
+            7: 5,
+            6: 2,
+            3: 1
+        }
+
+    def update_duplicate(self, w):
+        ind = int(w.objectName().replace('stretch_matrix_', ''))
+        dup_ind = self.stretch_matrix_duplicates.get(ind)
+        if dup_ind is not None:
+            dup = getattr(self.ui, f'stretch_matrix_{dup_ind}')
+            blocker = QSignalBlocker(dup)  # noqa: F841
+            dup.setValue(w.value())
+
+    def set_matrix_valid(self):
+        self.set_matrix_style_sheet('background-color: white')
+
+    def set_matrix_invalid(self):
+        self.set_matrix_style_sheet('background-color: red')
+
+    def set_matrix_style_sheet(self, s):
+        for w in self.stretch_matrix_widgets:
+            w.setStyleSheet(s)
+
+    @staticmethod
+    def convert_angle_convention(values, old_conv, new_conv):
+        values[:] = convert_angle_convention(values, old_conv, new_conv)
+
+    @property
+    def orientation(self):
+        # This automatically converts from Euler angle conventions
+        values = [x.value() for x in self.orientation_widgets]
+        if HexrdConfig().euler_angle_convention is not None:
+            convention = HexrdConfig().euler_angle_convention
+            self.convert_angle_convention(values, convention, None)
+
+        return values
+
+    @orientation.setter
+    def orientation(self, v):
+        # This automatically converts to Euler angle conventions
+        if HexrdConfig().euler_angle_convention is not None:
+            v = copy.deepcopy(v)
+            convention = HexrdConfig().euler_angle_convention
+            self.convert_angle_convention(v, None, convention)
+
+        for i, w in enumerate(self.orientation_widgets):
+            blocker = QSignalBlocker(w)  # noqa: F841
+            w.setValue(v[i])
+
+    @property
+    def position(self):
+        return [x.value() for x in self.position_widgets]
+
+    @position.setter
+    def position(self, v):
+        for i, w in enumerate(self.position_widgets):
+            blocker = QSignalBlocker(w)  # noqa: F841
+            w.setValue(v[i])
+
+    @property
+    def matrix_scaling(self):
+        scaling = np.array([np.sqrt(2)] * 9)
+        diagonal_indices = [0, 4, 8]
+        for i in diagonal_indices:
+            scaling[i] = 1
+
+        return scaling
+
+    @property
+    def inverse_stretch(self):
+        m = (self.stretch_matrix * self.matrix_scaling).reshape(3, 3)
+        m = np.linalg.inv(m).flatten()
+        indices = [0, 4, 8, 5, 2, 1]
+        return [m[i] for i in indices]
+
+    @inverse_stretch.setter
+    def inverse_stretch(self, v):
+        indices = [0, 5, 4, 5, 1, 3, 4, 3, 2]
+        m = np.array([v[i] for i in indices]).reshape(3, 3)
+        m = np.linalg.inv(m).flatten()
+        self.stretch_matrix = m / self.matrix_scaling
+
+    @property
+    def stretch_matrix(self):
+        return [x.value() for x in self.stretch_matrix_widgets]
+
+    @stretch_matrix.setter
+    def stretch_matrix(self, v):
+        for i, w in enumerate(self.stretch_matrix_widgets):
+            blocker = QSignalBlocker(w)  # noqa: F841
+            w.setValue(v[i])
+
+    @property
+    def orientation_widgets(self):
+        # Take advantage of the naming scheme
+        return [getattr(self.ui, f'orientation_{i}') for i in range(3)]
+
+    @property
+    def position_widgets(self):
+        # Take advantage of the naming scheme
+        return [getattr(self.ui, f'position_{i}') for i in range(3)]
+
+    @property
+    def stretch_matrix_widgets(self):
+        # Take advantage of the naming scheme
+        return [getattr(self.ui, f'stretch_matrix_{i}') for i in range(9)]
+
+    @property
+    def all_widgets(self):
+        return (
+            self.orientation_widgets +
+            self.position_widgets +
+            self.stretch_matrix_widgets
+        )
