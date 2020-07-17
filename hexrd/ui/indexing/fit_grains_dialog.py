ed651b01f7c11ab5ebb13dbe511b325e8139824a
John Tourtellott <john@tortuga2.home>
2020-07-17 16:22:37 -0400

    Implement add, delete, and move methods

--- hexrd/ui/indexing/fit_grains_dialog.py
+++ hexrd/ui/indexing/fit_grains_dialog.py
@@ -1,4 +1,4 @@
-from PySide2.QtCore import QObject, QSignalBlocker, Qt, Signal, Slot
+from PySide2.QtCore import QItemSelectionModel, QObject, QSignalBlocker, Qt, Signal, Slot
 from PySide2.QtWidgets import QHeaderView
 
 from hexrd.ui.hexrd_config import HexrdConfig
@@ -40,6 +40,10 @@ class FitGrainsDialog(QObject):
         self.ui.tth_max_enable.toggled.connect(self.on_tth_max_toggled)
         self.ui.tth_max_specify.toggled.connect(self.on_tth_specify_toggled)
         self.ui.tolerances_view.selectionModel().selectionChanged.connect(self.on_tolerances_select)
+        self.ui.add_row.clicked.connect(self.on_tolerances_add_row)
+        self.ui.delete_row.clicked.connect(self.on_tolerances_delete_row)
+        self.ui.move_up.clicked.connect(self.on_tolerances_move_up)
+        self.ui.move_down.clicked.connect(self.on_tolerances_move_down)
 
         result = self.ui.exec()
 
@@ -59,25 +63,49 @@ class FitGrainsDialog(QObject):
         return widgets
 
     @Slot()
+    def on_tolerances_add_row(self):
+        new_row_num = self.tolerances_model.rowCount()
+        self.tolerances_model.add_row()
+
+        # Select first column of new row
+        self.ui.tolerances_view.setFocus(Qt.OtherFocusReason)
+        self.ui.tolerances_view.selectionModel().clear()
+        model_index = self.tolerances_model.index(new_row_num, 0)
+        self.ui.tolerances_view.selectionModel().setCurrentIndex(
+            model_index, QItemSelectionModel.Select)
+        # Have to repaint - is that because we are in a modal dialog?
+        self.ui.tolerances_view.repaint(self.ui.tolerances_view.rect())
+
+    @Slot()
+    def on_tolerances_delete_row(self):
+        rows = self._get_selected_rows()
+        self.tolerances_model.delete_rows(rows)
+        self.ui.tolerances_view.selectionModel().clear()
+        self.ui.tolerances_view.repaint(self.ui.tolerances_view.rect())
+
+    @Slot()
+    def on_tolerances_move_down(self):
+        rows = self._get_selected_rows()
+        self.tolerances_model.move_rows(rows, 1)
+        self.ui.tolerances_view.selectionModel().clear()
+        self.ui.tolerances_view.repaint(self.ui.tolerances_view.rect())
+
+    @Slot()
+    def on_tolerances_move_up(self):
+        rows = self._get_selected_rows()
+        self.tolerances_model.move_rows(rows, -1)
+        self.ui.tolerances_view.selectionModel().clear()
+        self.ui.tolerances_view.repaint(self.ui.tolerances_view.rect())
+
+    @Slot()
     def on_tolerances_select(self):
         """Sets button enable states based on current selection"""
         delete_enable = False
         up_enable = False
         down_enable = False
 
-        # Make list of selected rows from the full selection
-        selection_model = self.ui.tolerances_view.selectionModel()
-        selection = selection_model.selection()
-        num_rows = self.tolerances_model.rowCount()
-        selected_rows = list()
-        for row in range(num_rows):
-            if selection_model.isRowSelected(row):
-                selected_rows.append(row)
-            elif selection_model.selectedColumns(row):
-                # Partial row selected - dont enable delete, up, down
-                selected_rows = None
-                break
-
+        # Get list of selected rows
+        selected_rows = self._get_selected_rows()
         if selected_rows:
             delete_enable = True
 
@@ -86,9 +114,8 @@ class FitGrainsDialog(QObject):
             is_contiguous = num_selected == selected_rows[-1] - selected_rows[0] + 1
             if is_contiguous:
                 up_enable = selected_rows[0] > 0
-                last_row = num_rows - 1
+                last_row = self.tolerances_model.rowCount() - 1
                 down_enable = selected_rows[-1] < last_row
-                print('  ', up_enable, last_row, down_enable)
 
         self.ui.delete_row.setEnabled(delete_enable)
         self.ui.move_up.setEnabled(up_enable)
@@ -136,3 +163,23 @@ class FitGrainsDialog(QObject):
 
         tolerances = config.get('tolerance')
         self.tolerances_model.update_from_config(tolerances)
+
+    def _get_selected_rows(self):
+        """Returns list of selected rows
+
+        Rows must be *exclusively* selected. If any partial rows are selected,
+        this method returns an empty list.
+        """
+        selection_model = self.ui.tolerances_view.selectionModel()
+        selection = selection_model.selection()
+        num_rows = self.tolerances_model.rowCount()
+        selected_rows = list()
+        for row in range(num_rows):
+            if selection_model.isRowSelected(row):
+                selected_rows.append(row)
+            elif selection_model.rowIntersectsSelection(row):
+                # Partial row is selected - return empty list
+                del selected_rows[:]
+                break
+
+        return selected_rows
--- hexrd/ui/indexing/fit_grains_tolerances_model.py
+++ hexrd/ui/indexing/fit_grains_tolerances_model.py
@@ -12,6 +12,8 @@ class FitGrainsToleranceModel(QAbstractTableModel):
         self.eta_tolerances = list()
         self.omega_tolerances = list()
 
+    # Override methods:
+
     def columnCount(self, parent=QModelIndex()):
         return 3
 
@@ -30,6 +32,7 @@ class FitGrainsToleranceModel(QAbstractTableModel):
 
         # Note that conventional [row][col] is reversed here
         element = table[column][row]
+
         return element
 
     def headerData(self, section, orientation, role=Qt.DisplayRole):
@@ -43,6 +46,50 @@ class FitGrainsToleranceModel(QAbstractTableModel):
         if parent.isValid():
             return 0
         return len(self.tth_tolerances)
+
+    # Custom methods:
+
+    def add_row(self):
+        new_row = self.rowCount()
+        self.beginInsertRows(QModelIndex(), new_row, new_row)
+        self.tth_tolerances.append(0.0)
+        self.eta_tolerances.append(0.0)
+        self.omega_tolerances.append(0.0)
+        self.endInsertRows()
+
+    def delete_rows(self, rows):
+        first = rows[0]
+        last = rows[-1]
+        self.beginRemoveRows(QModelIndex(), first, last)
+        for row in range(last, first-1, -1):
+            del self.tth_tolerances[row]
+            del self.eta_tolerances[row]
+            del self.omega_tolerances[row]
+        self.endRemoveRows()
+
+    def move_rows(self, rows, delta):
+        """Move rows in the table
+
+        @param delta: number of rows to move
+        """
+        first = rows[0]
+        last = rows[-1]
+        sourceIndex = QModelIndex()
+        destinationIndex = QModelIndex()
+        destination = first + delta
+        # Qt's destination depends on the direction of the move
+        offset = 1 if delta > 0 else 0
+        self.beginMoveRows(sourceIndex, first, last, sourceIndex, destination + offset)
+        table = [self.tth_tolerances, self.eta_tolerances, self.omega_tolerances]
+        for i in range(len(table)):
+            data = table[i]
+            moving_section = data[first:last+1]
+            remaining_list = data[:first] + data[last+1:]
+            first_section = remaining_list[:destination]
+            last_section = remaining_list[destination:]
+            table[i] = first_section + moving_section + last_section
+        self.tth_tolerances, self.eta_tolerances, self.omega_tolerances = table
+        self.endMoveRows()
 
     def update_from_config(self, config):
         # This method should generally be called *before* the instance is assigned to
