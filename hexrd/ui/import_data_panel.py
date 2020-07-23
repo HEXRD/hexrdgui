29f356a9e346214aa875fa3c7f67602a72e71024
Brianna Major <brianna.major@kitware.com>
2020-07-23 16:54:13 -0400

    Re-order the way images are loaded
    Update the ui and organization of the code to reflect the path of options for
    loading images: set instrument, set detector, load image, apply detector outline,
    position outline, complete masking and cropping of raw image.
    
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- hexrd/ui/import_data_panel.py
+++ hexrd/ui/import_data_panel.py
@@ -22,35 +22,12 @@ class ImportDataPanel(QObject):
         self.setup_connections()
     
     def setup_connections(self):
-        self.ui.load.clicked.connect(self.load_images)
         self.ui.instruments.currentIndexChanged.connect(
             self.instrument_selected)
         self.ui.detectors.currentIndexChanged.connect(self.detector_selected)
+        self.ui.load.clicked.connect(self.load_images)
         self.ui.trans.clicked.connect(self.setup_translate)
         self.ui.rotate.clicked.connect(self.setup_rotate)
-    
-    def load_images(self):
-        caption = HexrdConfig().images_dirtion = 'Select file(s)'
-        selected_files, selected_filter = QFileDialog.getOpenFileNames(
-            self.ui, caption, dir=HexrdConfig().images_dir)
-        if selected_files:
-            HexrdConfig().set_images_dir(selected_files[0])
-
-            # If it is a hdf5 file allow the user to select the path
-            ext = os.path.splitext(selected_files[0])[1]
-            if (ImageFileManager().is_hdf5(ext) and not
-                    ImageFileManager().path_exists(selected_files[0])):
-
-                ImageFileManager().path_prompt(selected_files[0])
-
-            selected_files = [[x] for x in selected_files]
-            ImageLoadManager().read_data(
-                selected_files, parent=self.ui)
-
-            files = [os.path.split(f[0])[1] for f in selected_files]
-            self.ui.files_label.setText(','.join(files))
-            self.ui.instruments.setEnabled(True)
-            self.ui.instrument_label.setEnabled(True)
 
     def instrument_selected(self, idx):
         TARDIS = ['None', 'IP2', 'IP3', 'IP4']
@@ -79,11 +56,28 @@ class ImportDataPanel(QObject):
         else:
             self.clear_boundry()
 
-    def clear_boundry(self):
-        if self.it is None:
-            return
-        self.it.clear()
-        self.it = None
+    def load_images(self):
+        caption = HexrdConfig().images_dirtion = 'Select file(s)'
+        selected_files, selected_filter = QFileDialog.getOpenFileNames(
+            self.ui, caption, dir=HexrdConfig().images_dir)
+        if selected_files:
+            HexrdConfig().set_images_dir(selected_files[0])
+
+            # If it is a hdf5 file allow the user to select the path
+            ext = os.path.splitext(selected_files[0])[1]
+            if (ImageFileManager().is_hdf5(ext) and not
+                    ImageFileManager().path_exists(selected_files[0])):
+
+                ImageFileManager().path_prompt(selected_files[0])
+
+            selected_files = [[x] for x in selected_files]
+            ImageLoadManager().read_data(
+                selected_files, parent=self.ui)
+
+            files = [os.path.split(f[0])[1] for f in selected_files]
+            self.ui.files_label.setText(','.join(files))
+            self.ui.instruments.setEnabled(True)
+            self.ui.instrument_label.setEnabled(True)
 
     def setup_translate(self):
         if self.it is not None:
@@ -94,3 +88,9 @@ class ImportDataPanel(QObject):
         if self.it is not None:
             self.it.disconnect_translate()
             self.it.connect_rotate()
+
+    def clear_boundry(self):
+        if self.it is None:
+            return
+        self.it.clear()
+        self.it = None
