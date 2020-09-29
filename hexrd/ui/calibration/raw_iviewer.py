310db62000a57d7807146b8427bd13e40831f1e4
Patrick Avery <patrick.avery@kitware.com>
2020-09-29 13:39:21 -0400

    Update overlays in raw view mode on detector edit
    When the detector transforms get modified, update the overlays
    in the raw view.
    
    This is a little slow to use in the slider view, because it is
    currently re-generating overlays for all of the raw images (not just
    the one that got changed). But maybe we can fix that in the future...
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/calibration/raw_iviewer.py
+++ hexrd/ui/calibration/raw_iviewer.py
@@ -1,5 +1,6 @@
 from hexrd.ui.constants import ViewType
 from hexrd.ui.create_hedm_instrument import create_hedm_instrument
+from hexrd.ui.hexrd_config import HexrdConfig
 from hexrd.ui.overlays import update_overlay_data
 
 
@@ -15,3 +16,13 @@ class InstrumentViewer:
 
     def update_overlay_data(self):
         update_overlay_data(self.instr, self.type)
+
+    def update_detector(self, det):
+        # First, convert to the "None" angle convention
+        iconfig = HexrdConfig().instrument_config_none_euler_convention
+
+        t_conf = iconfig['detectors'][det]['transform']
+        self.instr.detectors[det].tvec = t_conf['translation']
+        self.instr.detectors[det].tilt = t_conf['tilt']
+
+        # Since these are just individual images, no further updates are needed
