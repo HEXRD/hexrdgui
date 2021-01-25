6c30ee280927f04417d594c46e6773b4a9eb94b8
Brianna Major <brianna.major@kitware.com>
2021-01-25 10:00:07 -0500

    Do not reset load panel state on configuration change
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- hexrd/ui/load_panel.py
+++ hexrd/ui/load_panel.py
@@ -70,8 +70,6 @@ class LoadPanel(QObject):
         self.ui.file_options.resizeColumnsToContents()
 
     def setup_connections(self):
-        HexrdConfig().load_panel_state_reset.connect(
-            self.setup_processing_options)
         HexrdConfig().detectors_changed.connect(self.detectors_changed)
 
         self.ui.image_folder.clicked.connect(self.select_folder)
