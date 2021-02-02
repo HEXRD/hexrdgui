495fae6b9129c394df22dd5849507cfc9cb2a223
Brianna Major <brianna.major@kitware.com>
2021-02-02 16:56:33 -0500

    Keep track of dialog state
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- hexrd/ui/image_stack_dialog.py
+++ hexrd/ui/image_stack_dialog.py
@@ -20,9 +20,9 @@ class ImageStackDialog:
         self.detector = self.detectors[0]
         self.state = copy.copy(HexrdConfig().stack_state)
 
+        self.setup_state()
+        self.setup_gui()
         self.setup_connections()
-        self.select_files()
-        self.load_omega_from_file(self.state['omega-from-file'])
 
     def setup_connections(self):
         self.ui.select_directory.clicked.connect(self.select_directory)
@@ -39,15 +39,58 @@ class ImageStackDialog:
         self.ui.detectors.currentTextChanged.connect(self.change_detector)
         self.ui.total_frames.valueChanged.connect(self.update_delta_omega)
 
+    def setup_gui(self):
+        self.ui.detectors.addItems(self.detectors)
+        self.ui.current_directory.setText(self.state[self.detector]['directory'])
+        self.ui.current_directory.setToolTip(self.state[self.detector]['directory'])
+        self.ui.files.setText(self.state[self.detector]['files'])
+        self.ui.empty_frames.setValue(self.state['empty-frames'])
+        self.ui.max_file_frames.setValue(self.state['max-frame-file'])
+        self.ui.max_total_frames.setValue(self.state['max-frames'])
+        self.ui.omega_from_file.setChecked(self.state['omega-from-file'])
+        if self.state['omega']:
+            self.ui.omega_file.setText(self.state['omega'].split(' ')[-1])
+        self.ui.omega_from_file.setChecked(self.state['omega-from-file'])
+        self.ui.start_omega.setValue(self.state['ostart'])
+        self.ui.delta_omega.setValue(self.state['delta'])
+        self.ui.stop_omega.setValue(self.state['ostop'])
+
+    def setup_state(self):
+        if sorted(self.state.get('dets', [])) == sorted(self.detectors):
+            self.select_files()
+            self.load_omega_from_file(self.state['omega-from-file'])
+            return
+
+        self.state.clear()
+        self.state = {
+            'dets': self.detectors,
+            'empty-frames': 0,
+            'max-frame-file': 0,
+            'max-frames': 0,
+            'omega': '',
+            'ostart': 0,
+            'ostop': 360,
+            'delta': 360,
+            'omega-from-file': False,
+            'total-frames': 1,
+        }
+        for det in self.detectors:
+            self.state[det] = {
+                'directory': '',
+                'files': ''
+            }
+
     def select_directory(self):
         d = QFileDialog.getExistingDirectory(
                 self.ui, 'Select directory', self.parent_dir)
-        self.directory = d
+        self.state[self.detector]['directory'] = d
         self.parent_dir = d.rsplit('/', 1)[0]
         self.ui.current_directory.setText(d)
         self.ui.current_directory.setToolTip(d)
 
     def select_files(self):
+        self.state[self.detector]['files'] = self.ui.files.text()
+        directory = self.state[self.detector]['directory']
         if not (search := self.ui.files.text()):
             search = '*'
         files = list(Path(directory).glob(search))
@@ -55,9 +98,11 @@ class ImageStackDialog:
             ims = ImageFileManager().open_file(str(files[0]))
             frames = len(ims) if len(ims) else 1
             self.ui.total_frames.setValue(frames * len(files))
+            self.state['total-frames'] = frames
             self.set_ranges(frames, len(files))
 
     def load_omega_from_file(self, checked):
+        self.state['omega-from-file'] = checked
         self.ui.start_label.setDisabled(checked)
         self.ui.stop_label.setDisabled(checked)
         self.ui.delta_label.setDisabled(checked)
@@ -74,12 +119,15 @@ class ImageStackDialog:
             self.ui, 'Select file(s)',
             HexrdConfig().images_dir, 'NPY files (*.npy)')
         self.ui.omega_file.setText(omega_file)
+        self.state[self.detector]['omega'] = omega_file
 
     def update_stop_omega(self, delta):
         start = self.ui.start_omega.value()
         frames = self.ui.total_frames.value()
         stop = start + (delta * frames)
         self.ui.stop_omega.setValue(stop)
+        self.state['ostop'] = stop
+        self.state['delta'] = delta
 
     def update_delta_omega(self):
         start = self.ui.start_omega.value()
@@ -87,13 +135,23 @@ class ImageStackDialog:
         stop = self.ui.stop_omega.value()
         delta = abs(stop - start) / frames
         self.ui.delta_omega.setValue(delta)
+        self.state['ostart'] = start
+        self.state['ostop'] = stop
+        self.state['delta'] = delta
+
+    def set_empty_frames(self, value):
+        self.state['empty-frames'] = value
+
+    def set_max_file_frames(self, value):
+        self.state['max-frame-file'] = value
+
+    def set_max_total_frames(self, value):
+        self.state['max-frames'] = value
 
     def change_detector(self, det):
         self.detector = det
-        self.ui.current_directory.setText(
-            self.state[self.detector]['directory'])
-        self.ui.current_directory.setToolTip(
-            self.state[self.detector]['directory'])
+        self.ui.current_directory.setText(self.state[self.detector]['directory'])
+        self.ui.current_directory.setToolTip(self.state[self.detector]['directory'])
         self.ui.files.setText(self.state[self.detector]['files'])
 
     def set_ranges(self, frames, num_files):
