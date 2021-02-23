7a02f6aed543f9140f57121a0f1d6492c3766e61
Brianna Major <brianna.major@kitware.com>
2021-02-23 15:48:22 -0500

    Add table to list omega wedges
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- hexrd/ui/image_stack_dialog.py
+++ hexrd/ui/image_stack_dialog.py
@@ -32,16 +32,12 @@ class ImageStackDialog:
         self.ui.select_directory.clicked.connect(self.select_directory)
         self.ui.omega_from_file.toggled.connect(self.load_omega_from_file)
         self.ui.load_omega_file.clicked.connect(self.select_omega_file)
-        self.ui.start_omega.valueChanged.connect(self.update_delta_omega)
-        self.ui.stop_omega.valueChanged.connect(self.update_delta_omega)
-        self.ui.delta_omega.valueChanged.connect(self.update_stop_omega)
         self.ui.search.clicked.connect(self.search)
         self.ui.empty_frames.valueChanged.connect(self.set_empty_frames)
         self.ui.max_file_frames.valueChanged.connect(self.set_max_file_frames)
         self.ui.max_total_frames.valueChanged.connect(
             self.set_max_total_frames)
         self.ui.detectors.currentTextChanged.connect(self.change_detector)
-        self.ui.total_frames.valueChanged.connect(self.update_delta_omega)
         self.ui.files_by_selection.toggled.connect(self.file_selection_changed)
         self.ui.select_files.clicked.connect(self.select_files_manually)
 
@@ -58,9 +54,6 @@ class ImageStackDialog:
         if self.state['omega']:
             self.ui.omega_file.setText(self.state['omega'].split(' ')[-1])
         self.ui.omega_from_file.setChecked(self.state['omega-from-file'])
-        self.ui.start_omega.setValue(self.state['ostart'])
-        self.ui.delta_omega.setValue(self.state['delta'])
-        self.ui.stop_omega.setValue(self.state['ostop'])
         self.ui.files_by_selection.setChecked(self.state['manual-file'])
         self.ui.files_by_search.setChecked(not self.state['manual-file'])
         self.ui.file_count.setText(
@@ -80,9 +73,6 @@ class ImageStackDialog:
                 'max-frame-file': 0,
                 'max-frames': 0,
                 'omega': '',
-                'ostart': 0,
-                'ostop': 360,
-                'delta': 360,
                 'omega-from-file': False,
                 'total-frames': 1,
                 'manual-file': True,
@@ -130,14 +120,8 @@ class ImageStackDialog:
 
     def load_omega_from_file(self, checked):
         self.state['omega-from-file'] = checked
-        self.ui.start_label.setDisabled(checked)
-        self.ui.stop_label.setDisabled(checked)
-        self.ui.delta_label.setDisabled(checked)
-        self.ui.start_omega.setDisabled(checked)
-        self.ui.stop_omega.setDisabled(checked)
-        self.ui.delta_omega.setDisabled(checked)
-        self.ui.frames_label.setDisabled(checked)
-        self.ui.total_frames.setDisabled(checked)
+        self.ui.omega_wedges.setDisabled(checked)
+        self.ui.add_wedge.setDisabled(checked)
         self.ui.load_omega_file.setEnabled(checked)
         self.ui.omega_file.setEnabled(checked)
 
@@ -147,24 +131,6 @@ class ImageStackDialog:
             HexrdConfig().images_dir, 'NPY files (*.npy)')
         self.ui.omega_file.setText(omega_file)
         self.state[self.detector]['omega'] = omega_file
-
-    def update_stop_omega(self, delta):
-        start = self.ui.start_omega.value()
-        frames = self.ui.total_frames.value()
-        stop = start + (delta * frames)
-        self.ui.stop_omega.setValue(stop)
-        self.state['ostop'] = stop
-        self.state['delta'] = delta
-
-    def update_delta_omega(self):
-        start = self.ui.start_omega.value()
-        frames = self.ui.total_frames.value()
-        stop = self.ui.stop_omega.value()
-        delta = abs(stop - start) / frames
-        self.ui.delta_omega.setValue(delta)
-        self.state['ostart'] = start
-        self.state['ostop'] = stop
-        self.state['delta'] = delta
 
     def set_empty_frames(self, value):
         self.state['empty-frames'] = value
@@ -185,12 +151,6 @@ class ImageStackDialog:
         self.ui.empty_frames.setMaximum(frames)
         self.ui.max_file_frames.setMaximum(frames)
         self.ui.max_total_frames.setMaximum(frames * num_files)
-        self.ui.delta_omega.setMaximum(
-            (MAXIMUM_OMEGA_RANGE / (frames * num_files)))
-        self.ui.stop_omega.setMinimum(
-            (self.ui.start_omega.value() - MAXIMUM_OMEGA_RANGE))
-        self.ui.stop_omega.setMaximum(
-            (self.ui.start_omega.value() + MAXIMUM_OMEGA_RANGE))
 
     def file_selection_changed(self, checked):
         self.state['manual-file'] = checked
@@ -233,9 +193,6 @@ class ImageStackDialog:
             if self.state['omega-from-file']:
                 input_dict['meta']['omega'] = (
                     f'! load-numpy-array {self.state["omega"]}')
-            else:
-                input_dict['meta']['ostart'] = self.state['ostart']
-                input_dict['meta']['ostop'] = self.state['ostop']
             data = yaml.dump(input_dict).encode('utf-8')
             t.write(data)
             t.close()
@@ -245,15 +202,7 @@ class ImageStackDialog:
         return temp, imgs, num_files
 
     def get_omega_values(self, num_files):
-        start = self.state['ostart']
-        stop = self.state['ostop']
-        delta = self.state['delta']
-        frames = self.state['total-frames']
-        omega = np.linspace(
-            [start, start + delta],
-            [stop - delta, stop],
-            (frames * num_files))
-        return omega[:, 0], omega[:, 1]
+        return
 
     def build_data(self):
         HexrdConfig().stack_state = copy.deepcopy(self.state)
@@ -264,7 +213,6 @@ class ImageStackDialog:
             'yml_files': img_files,
             'omega_min': start,
             'omega_max': stop,
-            'delta': [self.state['delta']] * num_files,
             'empty_frames': self.state['empty-frames'],
             'total_frames': [self.state['total-frames']] * num_files,
         }
