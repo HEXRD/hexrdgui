8c26f189253165b7ec263935ed55e849db0ff20c
Brianna Major <brianna.major@kitware.com>
2021-03-12 09:15:39 -0500

    Check for lowercase file extension
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- hexrd/ui/save_images_dialog.py
+++ hexrd/ui/save_images_dialog.py
@@ -49,9 +49,9 @@ class SaveImagesDialog:
         for det in dets:
             filename = f'{self.ui.file_stem.text()}_{det}.{selected_format}'
             path = f'{self.parent_dir}/{filename}'
-            if selected_format.startswith('HDF5'):
+            if selected_format.startswith('hdf5'):
                 selected_format = 'hdf5'
-            elif selected_format.startswith('NPZ'):
+            elif selected_format.startswith('npz'):
                 selected_format = 'frame-cache'
 
             kwargs = {}
