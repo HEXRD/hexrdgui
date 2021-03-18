5874a1118c8a3450745c4d734caaff9e28a1e60f
Patrick Avery <patrick.avery@kitware.com>
2021-03-18 16:17:55 -0500

    Add automatic powder calibration
    This performs automatic calibration for a single powder overlay.
    
    It first automatically picks points, and then provides visual feedback
    in the canvases so that the users can see the points that were chosen.
    
    The user is then able to accept or reject the automatically picked points.
    
    This is taking over the "Powder Calibration" menu item. It is similar, but
    it also works for merged ranges, and it provides visual feedback.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/calibration/auto/powder_calibration_dialog.py
+++ hexrd/ui/calibration/auto/powder_calibration_dialog.py
@@ -11,16 +11,14 @@ class PowderCalibrationDialog:
         self.ui = loader.load_file('powder_calibration_dialog.ui', parent)
         self.ui.installEventFilter(enter_key_filter)
 
-        self.update_gui_from_config()
-        self.setup_connections()
+        self.update_gui()
 
-    def setup_connections(self):
-        pass
-
-    def update_gui_from_config(self):
-        tth_tol = HexrdConfig().config['calibration']['powder']['tth_tol']
-        eta_tol = HexrdConfig().config['calibration']['powder']['eta_tol']
-        pk_type = HexrdConfig().config['calibration']['powder']['pk_type']
+    def update_gui(self):
+        options = HexrdConfig().config['calibration']['powder']
+        tth_tol = options['tth_tol']
+        eta_tol = options['eta_tol']
+        pk_type = options['pk_type']
+        robust = options['use_robust_optimization']
 
         if pk_type == 'pvoigt':
             pk_type = 'PVoigt'
@@ -30,16 +28,23 @@ class PowderCalibrationDialog:
         self.ui.tth_tolerance.setValue(tth_tol)
         self.ui.eta_tolerance.setValue(eta_tol)
         self.ui.peak_fit_type.setCurrentText(pk_type)
+        self.ui.robust.setChecked(robust)
+
+    def update_config(self):
+        tth_tol = self.ui.tth_tolerance.value()
+        eta_tol = self.ui.eta_tolerance.value()
+        pk_type = self.ui.peak_fit_type.currentText().lower()
+        robust = self.ui.robust.isChecked()
+
+        options = HexrdConfig().config['calibration']['powder']
+        options['tth_tol'] = tth_tol
+        options['eta_tol'] = eta_tol
+        options['pk_type'] = pk_type
+        options['use_robust_optimization'] = robust
 
     def exec_(self):
         if not self.ui.exec_():
             return False
 
-        tth_tol = self.ui.tth_tolerance.value()
-        eta_tol = self.ui.eta_tolerance.value()
-        pk_type = self.ui.peak_fit_type.currentText().lower()
-
-        HexrdConfig().config['calibration']['powder']['tth_tol'] = tth_tol
-        HexrdConfig().config['calibration']['powder']['eta_tol'] = eta_tol
-        HexrdConfig().config['calibration']['powder']['pk_type'] = pk_type
+        self.update_config()
         return True
---
+++ hexrd/ui/calibration/auto/powder_runner.py
@@ -0,0 +1,245 @@
+import traceback
+
+import numpy as np
+
+from PySide2.QtCore import QObject, Qt, Signal
+from PySide2.QtWidgets import QCheckBox, QMessageBox
+
+from hexrd.ui.async_runner import AsyncRunner
+from hexrd.ui.create_hedm_instrument import create_hedm_instrument
+from hexrd.ui.constants import OverlayType
+from hexrd.ui.hexrd_config import HexrdConfig
+from hexrd.ui.overlays import default_overlay_refinements
+from hexrd.ui.utils import convert_tilt_convention
+
+from hexrd.ui.calibration.auto import (
+    InstrumentCalibrator,
+    PowderCalibrationDialog,
+    PowderCalibrator,
+)
+
+
+class PowderRunner(QObject):
+
+    finished = Signal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.parent = parent
+        self.async_runner = AsyncRunner(parent)
+
+    def clear(self):
+        self.remove_lines()
+
+    def run(self):
+        try:
+            self.validate()
+            self._run()
+        except Exception as e:
+            QMessageBox.critical(self.parent, 'HEXRD', f'Error: {e}')
+            traceback.print_exc()
+
+    def validate(self):
+        overlays = self.visible_powder_overlays
+        if len(overlays) != 1:
+            raise Exception('There must be exactly one visible powder overlay')
+
+        if np.count_nonzero(self.refinement_flags) == 0:
+            raise Exception('There are no refinable parameters')
+
+    def _run(self):
+        # First, have the user pick some options
+        if not PowderCalibrationDialog(self.parent).exec_():
+            # User canceled...
+            return
+
+        # The options they chose are saved here
+        options = HexrdConfig().config['calibration']['powder']
+        self.instr = create_hedm_instrument()
+
+        # Assume there is only one image in each image series for now...
+        img_dict = {k: x[0] for k, x in HexrdConfig().imageseries_dict.items()}
+
+        statuses = self.refinement_flags_without_overlays
+        self.cf = statuses
+        self.instr.calibration_flags = statuses
+
+        kwargs = {
+            'instr': self.instr,
+            'plane_data': self.material.planeData,
+            'img_dict': img_dict,
+            'flags': self.refinement_flags,
+            'tth_tol': options['tth_tol'],
+            'eta_tol': options['eta_tol'],
+            'pktype': options['pk_type'],
+        }
+
+        self.pc = PowderCalibrator(**kwargs)
+
+        # take a look at the ideal and "measured" line positions
+        self.fit_tth_tol = np.degrees(self.pc.tth_tol / 6)
+
+        self.extract_powder_lines()
+
+    def extract_powder_lines(self):
+        self.async_runner.progress_title = 'Auto picking points...'
+        self.async_runner.success_callback = self.extract_powder_lines_finished
+        self.async_runner.run(self.run_extract_powder_lines)
+
+    def run_extract_powder_lines(self):
+        kwargs = {
+            'fit_tth_tol': self.fit_tth_tol,
+            'int_cutoff': 0.01,
+        }
+        self.data_dict = self.pc._extract_powder_lines(**kwargs)
+
+    def extract_powder_lines_finished(self):
+        try:
+            self.draw_lines()
+            self.ask_if_lines_are_acceptable()
+        except Exception:
+            self.remove_lines()
+            raise
+
+    @property
+    def data_xys(self):
+        return {k: np.vstack(v)[:, :2] for k, v in self.data_dict.items()}
+
+    def show_lines(self, b):
+        self.draw_lines() if b else self.remove_lines()
+
+    def draw_lines(self):
+        HexrdConfig().auto_picked_data = self.data_xys
+
+    def remove_lines(self):
+        HexrdConfig().auto_picked_data = None
+
+    def ask_if_lines_are_acceptable(self):
+        msg = 'Perform calibration with the points drawn?'
+        box = QMessageBox(QMessageBox.Question, 'HEXRD', msg)
+        standard_buttons = QMessageBox.StandardButton
+        box.setStandardButtons(standard_buttons.Yes | standard_buttons.No)
+        box.setWindowModality(Qt.NonModal)
+
+        # Add a checkbox
+        cb = QCheckBox('Show auto picks?')
+        cb.setStyleSheet('margin-left:50%; margin-right:50%;')
+        cb.setChecked(True)
+        cb.toggled.connect(self.show_lines)
+
+        box.setCheckBox(cb)
+        box.show()
+
+        box.finished.connect(self.remove_lines)
+        box.accepted.connect(self.lines_accepted)
+
+        self._show_auto_picks_check_box = cb
+        self._ask_if_lines_are_acceptable_box = box
+
+    def lines_accepted(self):
+        # If accepted, run it
+        self.async_runner.progress_title = 'Running calibration...'
+        self.async_runner.success_callback = self.update_config
+        self.async_runner.run(self.run_calibration)
+
+    def run_calibration(self):
+        options = HexrdConfig().config['calibration']['powder']
+
+        use_robust_optimization = options['use_robust_optimization']
+
+        ic = InstrumentCalibrator(self.pc)
+        x0 = self.pc.instr.calibration_parameters[self.cf]
+        ic.run_calibration(conv_tol=0.0001,
+                           fit_tth_tol=self.fit_tth_tol,
+                           use_robust_optimization=use_robust_optimization)
+        x1 = self.pc.instr.calibration_parameters[self.cf]
+
+        results_message = 'Calibration Results:\n'
+        for params in np.vstack([x0, x1]).T:
+            results_message += f'{params[0]:6.3e}--->{params[1]:6.3e}\n'
+
+        print(results_message)
+
+        self.results_message = results_message
+
+    def update_config(self):
+        msg = 'Optimization successful!'
+        msg_box = QMessageBox(QMessageBox.Information, 'HEXRD', msg)
+        msg_box.setDetailedText(self.results_message)
+        msg_box.exec_()
+
+        instr = self.instr
+        iconfig = HexrdConfig().instrument_config_none_euler_convention
+        cal_crystal = iconfig.get('calibration_crystal')
+        output_dict = instr.write_config(calibration_dict=cal_crystal)
+
+        # Convert back to whatever convention we were using before
+        eac = HexrdConfig().euler_angle_convention
+        if eac is not None:
+            convert_tilt_convention(output_dict, None, eac)
+
+        # Add the saturation levels, as they seem to be missing
+        sl = 'saturation_level'
+        for det in output_dict['detectors'].keys():
+            output_dict['detectors'][det][sl] = iconfig['detectors'][det][sl]
+
+        # Save the previous iconfig to restore the statuses
+        prev_iconfig = HexrdConfig().config['instrument']
+
+        # Update the config
+        HexrdConfig().config['instrument'] = output_dict
+
+        # This adds in any missing keys. In particular, it is going to
+        # add in any "None" detector distortions
+        HexrdConfig().set_detector_defaults_if_missing()
+
+        # Add status values
+        HexrdConfig().add_status(output_dict)
+
+        # Set the previous statuses to be the current statuses
+        HexrdConfig().set_statuses_from_prev_iconfig(prev_iconfig)
+
+        self.finished.emit()
+
+    @property
+    def overlays(self):
+        return HexrdConfig().overlays
+
+    @property
+    def visible_overlays(self):
+        return [x for x in self.overlays if x['visible']]
+
+    @property
+    def visible_powder_overlays(self):
+        overlays = self.visible_overlays
+        return [x for x in overlays if x['type'] == OverlayType.powder]
+
+    @property
+    def active_overlay(self):
+        overlays = self.visible_powder_overlays
+        return overlays[0] if overlays else None
+
+    @property
+    def material(self):
+        overlay = self.active_overlay
+        return HexrdConfig().material(overlay['material']) if overlay else None
+
+    @property
+    def active_overlay_refinements(self):
+        return [x[1] for x in self.overlay_refinements(self.active_overlay)]
+
+    def overlay_refinements(self, overlay):
+        refinements = overlay.get('refinements')
+        if refinements is None:
+            refinements = default_overlay_refinements(overlay['type'])
+        return refinements
+
+    @property
+    def refinement_flags_without_overlays(self):
+        return HexrdConfig().get_statuses_instrument_format()
+
+    @property
+    def refinement_flags(self):
+        return np.hstack([self.refinement_flags_without_overlays,
+                          self.active_overlay_refinements])
