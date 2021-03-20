27f4e040f93c9d9c65bddfc4a5d9e55e90f6e580
Patrick Avery <patrick.avery@kitware.com>
2021-03-19 21:33:49 -0500

    Export raw data from eta omega maps viewer
    It was previously exporting out cleaned and filtered data.
    
    However, we have to be careful not to clean the data more
    than once, or else we will get different results, since it
    performs a thresholding based off of the median.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/indexing/ome_maps_viewer_dialog.py
+++ hexrd/ui/indexing/ome_maps_viewer_dialog.py
@@ -333,13 +333,14 @@ class OmeMapsViewerDialog(QObject):
         # cleaning on the original data on its own in
         # generate_orientation_fibers().
         self.raw_data = copy.deepcopy(d)
-        for map in self.raw_data.dataStore:
+        self.cleaned_data = copy.deepcopy(d)
+        for map in self.cleaned_data.dataStore:
             clean_map(map)
 
         # This data will have filters applied to it
         # We will make a shallow copy, and deep copy the data store
         # when filters are applied.
-        self._data = copy.copy(self.raw_data)
+        self._data = copy.copy(self.cleaned_data)
         self.reset_filters()
 
         self.update_extent()
@@ -480,7 +481,8 @@ class OmeMapsViewerDialog(QObject):
             if not selected_file.endswith('.npz'):
                 selected_file += '.npz'
 
-            self.data.save(selected_file)
+            # Save the raw data out...
+            self.raw_data.save(selected_file)
 
     @property
     def threshold(self):
@@ -596,7 +598,7 @@ class OmeMapsViewerDialog(QObject):
             name = '_dataStore'
         else:
             name = 'dataStore'
-        setattr(self.data, name, copy.deepcopy(self.raw_data.dataStore))
+        setattr(self.data, name, copy.deepcopy(self.cleaned_data.dataStore))
 
         # Make a fake config to pass to hexrd
         class Cfg:
