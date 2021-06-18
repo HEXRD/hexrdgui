ff816e50034333e59ebfcb7654a9df493b053774
Patrick Avery <patrick.avery@kitware.com>
2021-06-18 13:27:25 -0500

    Add basic testing infrastructure
    This includes a test that starts hexrdgui, loads an instrument config
    file, and then loads some data.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+
+import pytest
+
+from PySide2.QtCore import QSettings
+
+from hexrd.ui.main_window import MainWindow
+
+
+@pytest.fixture
+def example_repo_path():
+    if 'HEXRD_EXAMPLE_REPO_PATH' not in os.environ:
+        pytest.fail('Environment varable HEXRD_EXAMPLE_REPO_PATH not set!')
+
+    repo_path = os.environ['HEXRD_EXAMPLE_REPO_PATH']
+    return Path(repo_path)
+
+
+@pytest.fixture
+def single_ge_path(example_repo_path):
+    return example_repo_path / 'NIST_ruby/single_GE'
+
+
+@pytest.fixture
+def default_config_path(single_ge_path):
+    return single_ge_path / 'include/ge_detector.yml'
+
+
+@pytest.fixture
+def default_data_path(single_ge_path):
+    return single_ge_path / 'imageseries/RUBY_0000-fc_GE.npz'
+
+
+@pytest.fixture
+def main_window(qtbot):
+    # Clear the QSettings so we have a fresh run every time
+    QSettings().clear()
+
+    window = MainWindow()
+    qtbot.addWidget(window.ui)
+    return window
---
+++ tests/utils.py
@@ -0,0 +1,48 @@
+from contextlib import contextmanager
+
+from _pytest.monkeypatch import MonkeyPatch
+
+from PySide2.QtWidgets import QFileDialog
+
+monkeypatch = MonkeyPatch()
+
+
+@contextmanager
+def select_files_when_asked(filenames, filters=None):
+    if not isinstance(filenames, (list, tuple)):
+        filenames = [filenames]
+
+    if not isinstance(filters, (list, tuple)):
+        filters = [filters]
+
+    # Make sure they are strings
+    filenames = list(map(str, filenames))
+
+    def get_file_name(*args, **kwargs):
+        return filenames[0], filters[0]
+
+    def get_file_names(*args, **kwargs):
+        return filenames, filters
+
+    def get_dir_name(*args, **kwargs):
+        return filenames[0]
+
+    # Patch everything we need to in order to catch all types of
+    # file dialogs.
+    patches = {
+        'getOpenFileName': get_file_name,
+        'getOpenFileNames': get_file_names,
+        'getSaveFileName': get_file_name,
+        'getExistingDirectory': get_dir_name,
+    }
+
+    originals = {name: getattr(QFileDialog, name) for name in patches}
+
+    for name, func in patches.items():
+        monkeypatch.setattr(QFileDialog, name, func)
+
+    try:
+        yield
+    finally:
+        for name, func in originals.items():
+            monkeypatch.setattr(QFileDialog, name, func)
