281f64a48489ec4549faa149d2ea318f96562b48
Patrick Avery <patrick.avery@kitware.com>
2021-09-23 15:35:02 -0500

    Update overlays when oscillation stage changes
    This fixes an issue where overlays were not being updated properly
    when the oscillation stage would be changed.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/image_canvas.py
+++ hexrd/ui/image_canvas.py
@@ -72,6 +72,8 @@ class ImageCanvas(FigureCanvas):
         HexrdConfig().rerender_auto_picked_data.connect(
             self.draw_auto_picked_data)
         HexrdConfig().beam_vector_changed.connect(self.beam_vector_changed)
+        HexrdConfig().oscillation_stage_changed.connect(
+            self.oscillation_stage_changed)
         HexrdConfig().polar_masks_changed.connect(self.polar_masks_changed)
 
     def __del__(self):
@@ -479,6 +481,19 @@ class ImageCanvas(FigureCanvas):
 
         bvec = HexrdConfig().instrument_config['beam']['vector']
         self.iviewer.instr.beam_vector = (bvec['azimuth'], bvec['polar_angle'])
+        self.update_overlays()
+
+    def oscillation_stage_changed(self):
+        if not self.iviewer or not hasattr(self.iviewer, 'instr'):
+            return
+
+        # Need to update the parameters on the instrument
+        os_conf = HexrdConfig().instrument_config['oscillation_stage']
+        self.iviewer.instr.chi = os_conf['chi']
+        self.iviewer.instr.tvec = os_conf['translation']
+
+        # Re-draw all overlays from scratch
+        HexrdConfig().clear_overlay_data()
         self.update_overlays()
 
     def show_cartesian(self):
