Fix aggregated raw and polar rotation series
They seem to work properly now...

Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

hexrd/ui/overlays/rotation_series.py
@@ -121,10 +121,18 @@ class RotationSeriesSpotOverlay:
         point_groups = {}
         keys = ['data', 'ranges']
         for det_key, psim in sim_data.items():
+            panel = self.instrument.detectors[det_key]
             point_groups[det_key] = {key: [] for key in keys}
             valid_ids, valid_hkls, valid_angs, valid_xys, ang_pixel_size = psim
+            angles, omegas = np.hsplit(valid_angs[0], [2])
             if display_mode == ViewType.polar:
-                point_groups[det_key]['data'] = valid_angs[0]
+                point_groups[det_key]['data'] = np.degrees(angles)
             elif display_mode in [ViewType.raw, ViewType.cartesian]:
-                point_groups[det_key]['data'] = valid_xys[0]
+                result = valid_xys[0]
+                if display_mode == ViewType.raw:
+                    # If raw, convert to pixels
+                    result[:] = panel.cartToPixel(result)
+                    result[:, [0, 1]] = result[:, [1, 0]]
+                point_groups[det_key]['data'] = result
+
         return point_groups