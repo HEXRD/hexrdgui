000b7d9973f4016da86a29c07afa6a1cb9f44001
Joel Bernier <joelvbernier@me.com>
2021-12-22 21:12:10 -0800

    some fixes to the auto powder calibrator

--- hexrd/ui/calibration/auto/powder_calibrator.py
+++ hexrd/ui/calibration/auto/powder_calibrator.py
@@ -188,7 +188,7 @@ class PowderCalibrator(object):
                         tth_centers = angs[0]
                         eta_ref = angs[1]
                         int1d = intensities[0]
-                        
+
                         # peak profile fitting
                         if len(dsp0[i_ring]) == 1:
                             p0 = fitpeak.estimate_pk_parms_1d(
@@ -206,15 +206,12 @@ class PowderCalibrator(object):
                             if p[0] < int_cutoff or center_err > fit_tth_tol:
                                 tmp.append(np.empty((0, nfields_data)))
                                 continue
-                            xy_meas = panel.angles_to_cart(
-                                [[tth_meas, eta_ref], ]
-                            )
 
-                            # distortion
-                            if panel.distortion is not None:
-                                xy_meas = panel.distortion.apply_inverse(
-                                    xy_meas
-                                )
+                            xy_meas = panel.angles_to_cart(
+                                [[tth_meas, eta_ref], ],
+                                tvec_s=self.instr.tvec,
+                                apply_distortion=True
+                            )
 
                             # cat results
                             tmp.append(
@@ -265,16 +262,12 @@ class PowderCalibrator(object):
                             ):
                                 tmp.append(np.empty((0, nfields_data)))
                                 continue
+
                             xy_meas = panel.angles_to_cart(
-                                np.vstack(
-                                    [tth_meas, eta_ref_tile]
-                                ).T
+                                np.vstack([tth_meas, eta_ref_tile]).T,
+                                tvec_s=self.instr.tvec,
+                                apply_distortion=True
                             )
-                            # distortion
-                            if panel.distortion is not None:
-                                xy_meas = panel.distortion.apply_inverse(
-                                    xy_meas
-                                )
 
                             # cat results
                             tmp.append(
@@ -297,16 +290,17 @@ class PowderCalibrator(object):
     def _evaluate(self, reduced_params, data_dict, output='residual'):
         """
         """
-        # need this for dsp
-        bmat = self.plane_data.latVecOps['B']
-
-        # first update instrument from input parameters
+        # first update full parameters from input reduced parameters
+        # TODO: make this process a class method
         full_params = np.asarray(self.full_params)
         full_params[self.flags] = reduced_params
 
+        # !!! properties update from parameters
         self.instr.update_from_parameter_list(full_params[:self.npi])
         self.params = full_params[self.npi:]
 
+        # need this for dsp
+        bmat = self.plane_data.latVecOps['B']
         wlen = self.instr.beam_wavelength
 
         # build residual
@@ -314,26 +308,48 @@ class PowderCalibrator(object):
         for det_key, panel in self.instr.detectors.items():
             if len(data_dict[det_key]) == 0:
                 continue
-
             pdata = np.vstack(data_dict[det_key])
             if len(pdata) > 0:
+                """
+                Here is the strategy:
+                    1. remap the feature points from raw cartesian to
+                       (tth, eta) under the current mapping
+                    2. use the lattice and hkls to calculate the ideal tth0
+                    3. push the (tth0, eta) values back through the mapping to
+                       raw cartesian coordinates
+                    4. build residual on the measured and recalculated (x, y)
+                """
+                # push measured (x, y) ring points through current mapping
+                # to (tth, eta)
+                meas_xy = pdata[:, :2]
+                updates_angles, _ = panel.cart_to_angles(
+                    meas_xy,
+                    tvec_s=self.instr.tvec,
+                    apply_distortion=True
+                )
+
+                # derive ideal tth positions from additional ring point info
                 hkls = pdata[:, 3:6]
                 gvecs = np.dot(hkls, bmat.T)
                 dsp0 = 1./np.sqrt(np.sum(gvecs*gvecs, axis=1))
-                # dsp0 = pdata[:, -2]
-                eta0 = pdata[:, -1]
-
-                # derive reference tth
                 tth0 = 2.*np.arcsin(0.5*wlen/dsp0)
-                calc_xy = panel.angles_to_cart(np.vstack([tth0, eta0]).T)
 
-                # distortion if applicable, from ideal --> warped
-                if panel.distortion is not None:
-                    calc_xy = panel.distortion.apply_inverse(calc_xy)
+                # !!! get eta from mapped markers rather than ref
+                # eta0 = pdata[:, -1]
+                eta0 = updates_angles[:, 1]
 
+                # map updated (tth0, eta0) back to cartesian coordinates
+                tth_eta = np.vstack([tth0, eta0]).T
+                calc_xy = panel.angles_to_cart(
+                    tth_eta,
+                    tvec_s=self.instr.tvec,
+                    apply_distortion=True
+                )
+
+                # output
                 if output == 'residual':
                     retval.append(
-                        (pdata[:, :2].flatten() - calc_xy.flatten())
+                        meas_xy.flatten() - calc_xy.flatten()
                     )
                 elif output == 'model':
                     retval.append(
