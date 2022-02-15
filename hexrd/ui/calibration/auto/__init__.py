bacbb3659d26d13685b3387cbdadc6d583df3440
Patrick Avery <patrick.avery@kitware.com>
2022-02-15 13:48:56 -0600

    Remove hexrdgui's auto calibrators
    We are now using the ones in hexrd...
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/calibration/auto/__init__.py
+++ hexrd/ui/calibration/auto/__init__.py
@@ -1,11 +1,7 @@
-from .instrument_calibrator import InstrumentCalibrator
 from .powder_calibration_dialog import PowderCalibrationDialog
-from .powder_calibrator import PowderCalibrator
 from .powder_runner import PowderRunner
 
 __all__ = [
-    'InstrumentCalibrator',
     'PowderCalibrationDialog',
-    'PowderCalibrator',
     'PowderRunner',
 ]
