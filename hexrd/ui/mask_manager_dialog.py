d315f57f06343084b47f74b8486c0adbfa8b4456
Brianna Major <brianna.major@kitware.com>
2022-06-14 16:00:02 -0400

    Set visible masks as panel buffer from mask manager
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- hexrd/ui/mask_manager_dialog.py
+++ hexrd/ui/mask_manager_dialog.py
@@ -1,3 +1,4 @@
+import copy
 from hexrd.ui.create_polar_mask import rebuild_polar_masks
 from hexrd.ui.create_raw_mask import rebuild_raw_masks
 import os
@@ -76,10 +77,11 @@ class MaskManagerDialog(QObject):
             self.context_menu_event)
         self.ui.export_masks.clicked.connect(self.write_all_masks)
         self.ui.import_masks.clicked.connect(self.import_masks)
+        self.ui.panel_buffer.clicked.connect(self.masks_panel_to_buffer)
+
         HexrdConfig().mode_threshold_mask_changed.connect(
             self.update_masks_list)
         HexrdConfig().detectors_changed.connect(self.clear_masks)
-
         HexrdConfig().save_state.connect(self.save_state)
         HexrdConfig().load_state.connect(self.load_state)
         HexrdConfig().state_loaded.connect(self.rebuild_masks)
@@ -306,3 +308,29 @@ class MaskManagerDialog(QObject):
             HexrdConfig().polar_masks_changed.emit()
 
         self.update_masks_list('raw')
+
+    def masks_panel_to_buffer(self):
+        # Set the visible masks as the panel buffer(s)
+        # We must ensure that we are using raw masks
+        from hexrd.ui.create_raw_mask import rebuild_raw_masks
+        prev_masks = copy.deepcopy(HexrdConfig().masks)
+        try:
+            rebuild_raw_masks()
+            masks = copy.deepcopy(HexrdConfig().masks)
+        finally:
+            HexrdConfig().masks = prev_masks
+
+        for name, img in HexrdConfig().images_dict.items():
+            final_mask = np.ones(img.shape, dtype=bool)
+            for mask_name, data in masks.items():
+                if mask_name not in HexrdConfig().visible_masks:
+                    continue
+
+                for det, mask in data:
+                    if det == name:
+                        final_mask = np.logical_and(final_mask, mask)
+
+            detector_config = HexrdConfig().detector(name)
+            buffer_default = {'status': 0}
+            buffer = detector_config.setdefault('buffer', buffer_default)
+            buffer['value'] = final_mask
