3f03755bfe602cf73ff2a43f53b2a86b5b1bd3e5
Patrick Avery <patrick.avery@kitware.com>
2022-08-29 10:42:55 -0400

    Connect GUI with overlay config for pinhole work
    This seems to be working so far.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/powder_overlay_editor.py
+++ hexrd/ui/powder_overlay_editor.py
@@ -2,7 +2,7 @@ import copy
 
 import numpy as np
 
-from PySide2.QtWidgets import QCheckBox, QDoubleSpinBox, QMessageBox
+from PySide2.QtWidgets import QCheckBox, QComboBox, QDoubleSpinBox, QMessageBox
 
 from hexrd.ui.hexrd_config import HexrdConfig
 from hexrd.ui.reflections_table import ReflectionsTable
@@ -31,6 +31,8 @@ class PowderOverlayEditor:
                 w.valueChanged.connect(self.update_config)
             elif isinstance(w, QCheckBox):
                 w.toggled.connect(self.update_config)
+            elif isinstance(w, QComboBox):
+                w.currentIndexChanged.connect(self.update_config)
 
         self.ui.enable_width.toggled.connect(self.update_enable_states)
         self.refinements_selector.selection_changed.connect(
@@ -42,7 +44,7 @@ class PowderOverlayEditor:
             self.material_tth_width_modified_externally)
 
         self.ui.distortion_type.currentIndexChanged.connect(
-            self.validate_distortion_type)
+            self.distortion_type_changed)
         self.ui.pinhole_correction_type.currentIndexChanged.connect(
             self.validate_pinhole_correction_type)
 
@@ -103,6 +105,8 @@ class PowderOverlayEditor:
         with block_signals(*self.widgets):
             self.tth_width_gui = self.tth_width_config
             self.offset_gui = self.offset_config
+            self.distortion_type_gui = self.distortion_type_config
+            self.distortion_kwargs_gui = self.distortion_kwargs_config
             self.refinements_with_labels = self.overlay.refinements_with_labels
 
             self.update_enable_states()
@@ -111,6 +115,8 @@ class PowderOverlayEditor:
     def update_config(self):
         self.tth_width_config = self.tth_width_gui
         self.offset_config = self.offset_gui
+        self.distortion_type_config = self.distortion_type_gui
+        self.distortion_kwargs_config = self.distortion_kwargs_gui
 
         self.overlay.update_needed = True
         HexrdConfig().overlay_config_changed.emit()
@@ -176,15 +182,136 @@ class PowderOverlayEditor:
             w.setValue(v[i])
 
     @property
+    def distortion_type_config(self):
+        if self.overlay is None:
+            return None
+
+        return self.overlay.tth_distortion_type
+
+    @distortion_type_config.setter
+    def distortion_type_config(self, v):
+        if self.overlay is None:
+            return
+
+        self.overlay.tth_distortion_type = v
+
+    @property
+    def distortion_type_gui(self):
+        if self.ui.distortion_type.currentText() == 'Offset':
+            return None
+
+        v = self.ui.pinhole_correction_type.currentText()
+
+        conversions = {
+            'Sample Layer': 'SampleLayerDistortion',
+        }
+        if v in conversions:
+            v = conversions[v]
+
+        return v
+
+    @distortion_type_gui.setter
+    def distortion_type_gui(self, v):
+        widgets = [self.ui.distortion_type, self.ui.pinhole_correction_type]
+        with block_signals(*widgets):
+            if v is None:
+                self.ui.distortion_type.setCurrentText('Offset')
+                idx = self.ui.distortion_type.currentIndex()
+                self.ui.distortion_tab_widget.setCurrentIndex(idx)
+                return
+
+            self.ui.distortion_type.setCurrentText('Pinhole Camera Correction')
+            idx = self.ui.distortion_type.currentIndex()
+            self.ui.distortion_tab_widget.setCurrentIndex(idx)
+
+            conversions = {
+                'SampleLayerDistortion': 'Sample Layer',
+            }
+            if v in conversions:
+                v = conversions[v]
+
+            self.ui.pinhole_correction_type.setCurrentText(v)
+            idx = self.ui.pinhole_correction_type.currentIndex()
+            self.ui.pinhole_correction_type_tab_widget.setCurrentIndex(idx)
+
+    @property
+    def distortion_kwargs_config(self):
+        if self.overlay is None:
+            return
+
+        return self.overlay.tth_distortion_kwargs
+
+    @distortion_kwargs_config.setter
+    def distortion_kwargs_config(self, v):
+        if self.overlay is None:
+            return
+
+        self.overlay.tth_distortion_kwargs = v
+
+    @property
+    def distortion_kwargs_gui(self):
+        dtype = self.distortion_type_gui
+        if dtype is None:
+            return None
+        elif dtype == 'SampleLayerDistortion':
+            return {
+                'layer_standoff': self.ui.sl_layer_standoff.value(),
+                'layer_thickness': self.ui.sl_layer_thickness.value(),
+                'pinhole_thickness': self.ui.sl_pinhole_thickness.value(),
+            }
+        elif dtype == 'Pinhole':
+            return {
+                'diameter': self.ui.ph_diameter.value(),
+                'thickness': self.ui.ph_thickness.value(),
+            }
+
+        raise Exception(f'Not implemented for: {dtype}')
+
+    @distortion_kwargs_gui.setter
+    def distortion_kwargs_gui(self, v):
+        dtype = self.distortion_type_gui
+        if dtype is None:
+            return
+        elif dtype == 'SampleLayerDistortion':
+            self.ui.sl_layer_standoff.setValue(v.get('layer_standoff', 0))
+            self.ui.sl_layer_thickness.setValue(v.get('layer_thickness', 0))
+            self.ui.sl_pinhole_thickness.setValue(v.get('pinhole_thickness',
+                                                        0))
+        elif dtype == 'Pinhole':
+            self.ui.ph_diameter.setValue(v.get('diameter', 0))
+            self.ui.thickness.setValue(v.get('thickness', 0))
+        else:
+            raise Exception(f'Not implemented for: {dtype}')
+
+    @property
     def offset_widgets(self):
         return [getattr(self.ui, f'offset_{i}') for i in range(3)]
 
     @property
+    def sample_layer_widgets(self):
+        widgets = [
+            'sl_layer_standoff',
+            'sl_layer_thickness',
+            'sl_pinhole_thickness',
+        ]
+        return [getattr(self.ui, w) for w in widgets]
+
+    @property
+    def pinhole_widgets(self):
+        return [self.ui.ph_diameter, self.ui.ph_thickness]
+
+    @property
     def widgets(self):
+        distortion_widgets = (
+            self.offset_widgets +
+            self.sample_layer_widgets +
+            self.pinhole_widgets +
+            [self.ui.distortion_type, self.ui.pinhole_correction_type]
+        )
         return [
             self.ui.enable_width,
             self.ui.tth_width
-        ] + self.offset_widgets
+        ] + distortion_widgets
 
     def material_tth_width_modified_externally(self, material_name):
         if not self.material:
@@ -223,6 +350,15 @@ class PowderOverlayEditor:
     @pinhole_correction_type.setter
     def pinhole_correction_type(self, v):
         self.ui.pinhole_correction_type.setCurrentText(v)
+
+    def reset_offsets(self):
+        self.offset_gui = [0, 0, 0]
+        self.offset_config = [0, 0, 0]
+
+    def distortion_type_changed(self):
+        # If the distortion type is changed, zero the offsets
+        self.reset_offsets()
+        self.validate_distortion_type()
 
     def validate_distortion_type(self):
         if self.distortion_type == 'Pinhole Camera Correction':
