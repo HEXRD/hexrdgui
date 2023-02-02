eac1d64ec59e7a549562c0f4d05fcf58eb193484
Patrick Avery <patrick.avery@kitware.com>
2023-02-02 04:50:17 -0500

    Draw detector borders in stereo view
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/calibration/stereo_plot.py
+++ hexrd/ui/calibration/stereo_plot.py
@@ -1,9 +1,13 @@
 import numpy as np
 
+from hexrd import constants as ct
+from hexrd.transforms.xfcapi import detectorXYToGvec
+
 from hexrd.ui.constants import ViewType
 from hexrd.ui.create_hedm_instrument import create_hedm_instrument
 from hexrd.ui.hexrd_config import HexrdConfig
 from hexrd.ui.overlays import update_overlay_data
+from hexrd.ui.utils.conversions import angles_to_stereo
 
 from .polarview import PolarView
 from .stereo_project import stereo_project, stereo_projection_of_polar_view
@@ -39,11 +43,47 @@ class InstrumentViewer:
     def project_from_polar(self):
         return HexrdConfig().stereo_project_from_polar
 
+    def detector_borders(self, det):
+        panel = self.instr.detectors[det]
+
+        # First, create the polar view version of the borders
+        # (this skips some things like trimming based upon tth/eta min/max)
+        row_vec, col_vec = panel.row_pixel_vec, panel.col_pixel_vec
+        x_start, x_stop = col_vec[0], col_vec[-1]
+        y_start, y_stop = row_vec[0], row_vec[-1]
+
+        # Create the borders in Cartesian
+        borders = [
+            [[x, y_start] for x in col_vec],
+            [[x, y_stop] for x in col_vec],
+            [[x_start, y] for y in row_vec],
+            [[x_stop, y] for y in row_vec]
+        ]
+
+        # Convert each border to angles, then stereo
+        for i, border in enumerate(borders):
+            angles, _ = detectorXYToGvec(
+                border, panel.rmat, ct.identity_3x3,
+                panel.tvec, ct.zeros_3, ct.zeros_3,
+                beamVec=panel.bvec, etaVec=panel.evec)
+            angles = np.asarray(angles)
+
+            # Swap positions of tth and eta
+            angles[:, [0, 1]] = angles[:, [1, 0]]
+
+            # Need to transpose angles before and after
+            borders[i] = angles_to_stereo(angles.T, self.instr,
+                                          self.stereo_size).T
+
+        return borders
+
     @property
     def all_detector_borders(self):
-        # return self.pv.all_detector_borders
-        # Until we compute this, just return an empty dict of detectors
-        return {k: [] for k in self.instr.detectors}
+        borders = {}
+        for key in self.instr.detectors:
+            borders[key] = self.detector_borders(key)
+
+        return borders
 
     def draw_stereo(self):
         if self.project_from_polar:
