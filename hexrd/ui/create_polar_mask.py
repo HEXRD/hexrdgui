Make several fixes to polar masking
This adds several fixes, including:

1. Slightly adjusting the "snap" values in the rectangular mask (so that they don't go out of bounds on conversion)
2. Increasing the required gap to assume the mask is split across eta
3. Adding logic to handle a single gap (there must be two gaps, so we then look for the second largest gap)

Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

hexrd/ui/create_polar_mask.py
@@ -34,22 +34,28 @@ def create_polar_mask(name, line_data):
     # If any consecutive pixel coordinates for the mask are greater than this
     # distance apart (in eta), then that means the mask is being split
     # above/below the image, and we need to split the mask up.
-    eta_max_pix_diff = num_pix_eta / 2
+    eta_max_pix_diff = num_pix_eta * 0.95
 
     # Generate masks from line data
     final_mask = np.ones(shape, dtype=bool)
     for line in line_data:
-        tth = np.asarray([point[0] for point in line])
-        eta = np.asarray([point[1] for point in line])
+        # Remove any nans
+        line = line[~np.isnan(line.min(axis=1))]
+        tth = line[:, 0]
+        eta = line[:, 1]
 
         j_col = np.floor((tth - np.degrees(pv.tth_min)) / pv.tth_pixel_size)
         i_row = np.floor((eta - np.degrees(pv.eta_min)) / pv.eta_pixel_size)
 
         gaps, = np.nonzero(np.abs(np.diff(i_row)) > eta_max_pix_diff)
-        if gaps.size == 0:
-            # Just one mask
-            masks = [_pixel_perimeter_to_mask(i_row, j_col, shape)]
-        else:
+
+        if gaps.size == 1:
+            # Add an extra gap at the second biggest gap
+            idx = eta.shape[0] - 3
+            second_biggest_gap, = np.where(np.argsort(np.diff(eta)) == idx)
+            gaps = np.sort(np.hstack((gaps, second_biggest_gap)))
+
+        if gaps.size == 2:
             # This mask is split between the top and bottom of the image.
             # We need to split it up into two polygons.
 
@@ -68,6 +74,9 @@ def create_polar_mask(name, line_data):
                 _pixel_perimeter_to_mask(i_row1, j_col1, shape),
                 _pixel_perimeter_to_mask(i_row2, j_col2, shape),
             ]
+        else:
+            # Just one mask
+            masks = [_pixel_perimeter_to_mask(i_row, j_col, shape)]
 
         for mask in masks:
             final_mask = np.logical_and(final_mask, mask)