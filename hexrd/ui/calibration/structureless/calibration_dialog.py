93af29935a43610e571c3e0880d1a2971c6149a0
Patrick Avery <patrick.avery@kitware.com>
2023-06-13 10:40:50 -0500

    Add help buttons for structureless calibration
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/ui/calibration/structureless/calibration_dialog.py
+++ hexrd/ui/calibration/structureless/calibration_dialog.py
@@ -10,6 +10,7 @@ from hexrd.ui.tree_views.multi_column_dict_tree_view import (
 )
 from hexrd.ui.pinhole_correction_editor import PinholeCorrectionEditor
 from hexrd.ui.ui_loader import UiLoader
+from hexrd.ui.utils.dialog import add_help_url
 
 import hexrd.ui.resources.calibration
 
@@ -38,6 +39,7 @@ class StructurelessCalibrationDialog(QObject):
                                    parent)
 
         self.ui.setWindowFlags(self.ui.windowFlags() | Qt.Tool)
+        add_help_url(self.ui.button_box, 'calibration/structureless/')
 
         self.pinhole_correction_editor = PinholeCorrectionEditor(self.ui)
         editor = self.pinhole_correction_editor
