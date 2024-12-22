951ee79c6f868d5075c25a29ccad30df328fcbad
Brianna Major <brianna.major@taloid.khq.kitware.com>
2024-12-22 12:14:20 -0500

    Refactor median kernel into its own dialog
    Signed-off-by: Brianna Major <brianna.major@taloid.khq.kitware.com>

--- hexrdgui/llnl_import_tool_dialog.py
+++ hexrdgui/llnl_import_tool_dialog.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import re
+from hexrdgui.median_filter_dialog import MedianFilterDialog
 import numpy as np
 import yaml
 import tempfile
@@ -74,7 +75,6 @@ class LLNLImportToolDialog(QObject):
         self.canvas = parent.image_tab_widget.active_canvas
         self.physics_package_manager = physics_package_manager
         self.detector_images = {}
-        self.median_filter_action = parent.action_apply_median_filter
 
         # Disable these by default.
         # If we disable these in Qt Designer, there are some weird bugs
@@ -910,10 +910,14 @@ class LLNLImportToolDialog(QObject):
         HexrdConfig().enable_canvas_toolbar.emit(True)
         self.cmap.block_updates(False)
 
-        # Allow user to change median filter kernel
-        self.median_filter_action.setChecked(True)
-        # Prompt user to update the physics package
-        self.physics_package_manager.show()
+        HexrdConfig().apply_median_filter_correction = True
+        d = MedianFilterDialog(self.ui.parent())
+        if self.instrument == 'FIDDLE':
+            # Allow user to change kernel before applying median filter
+            d.show()
+        if self.instrument != 'FIDDLE' or d.exec():
+            # Prompt user to update the physics package
+            self.physics_package_manager.show()
 
     def show(self):
         self.ui.show()
