5af585c7e0c833c2026925bf668382228e8154dd
Saransh Singh <saransh13@gmail.com>
2025-05-13 15:11:48 -0700

    include numpy import

--- hexrdgui/constants.py
+++ hexrdgui/constants.py
@@ -4,6 +4,8 @@ from hexrd import constants
 
 from matplotlib import cm
 
+import numpy as np
+
 # Wavelength to kilo electron volt conversion
 WAVELENGTH_TO_KEV = constants.keVToAngstrom(1.)
 KEV_TO_WAVELENGTH = constants.keVToAngstrom(1.)
